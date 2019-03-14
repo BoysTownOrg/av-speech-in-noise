49f2b00ac0d55264a9b94e6245101aa15ce81797
Seth Bashford <seth.bashford@boystown.org>
2019-03-14 09:18:48 -0500

    add test

--- src/google-tests/RandomizedMaskerPlayerTests.cpp
+++ src/google-tests/RandomizedMaskerPlayerTests.cpp
@@ -4,6 +4,7 @@ class VideoPlayer {
 public:
     virtual ~VideoPlayer() = default;
     virtual bool playing() = 0;
+    virtual void loadFile(std::string) = 0;
 };
 
 class RandomizedMaskerPlayer : public recognition_test::MaskerPlayer {
@@ -30,16 +31,18 @@ public:
     
     }
     void loadFile(std::string filePath) override {
-    
+        player->loadFile(std::move(filePath));
     }
     bool playing() override {
         return player->playing();
     }
 };
 
+#include "assert-utility.h"
 #include <gtest/gtest.h>
 
 class VideoPlayerStub : public VideoPlayer {
+    std::string filePath_{};
     bool playing_{};
 public:
     void setPlaying() {
@@ -48,6 +51,14 @@ public:
     
     bool playing() override {
         return playing_;
+    }
+    
+    void loadFile(std::string s) override {
+        filePath_ = std::move(s);
+    }
+    
+    auto filePath() const {
+        return filePath_;
     }
 };
 
@@ -60,4 +71,9 @@ protected:
 TEST_F(RandomizedMaskerPlayerTests, playingWhenVideoPlayerPlaying) {
     videoPlayer.setPlaying();
     EXPECT_TRUE(player.playing());
+}
+
+TEST_F(RandomizedMaskerPlayerTests, loadFileLoadsVideoFile) {
+    player.loadFile("a");
+    assertEqual("a", videoPlayer.filePath());
 }
