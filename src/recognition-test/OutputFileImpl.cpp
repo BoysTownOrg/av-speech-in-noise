f712b0b15e9c14303fad20aac63a0297f3d0437c
Seth Bashford <seth.bashford@boystown.org>
2019-03-27 10:44:49 -0500

    refactor

--- src/recognition-test/OutputFileImpl.cpp
+++ src/recognition-test/OutputFileImpl.cpp
@@ -1,8 +1,53 @@
-//
-//  OutputFile.cpp
-//  recognition-test
-//
-//  Created by Bashford, Seth on 3/27/19.
-//
-
 #include "OutputFileImpl.hpp"
+#include <sstream>
+
+namespace recognition_test {
+    class TrialStream {
+        std::stringstream stream;
+    public:
+        template<typename T>
+        void insert(T item) {
+            stream << item;
+        }
+        
+        void insertCommaAndSpace() {
+            stream << ", ";
+        }
+        
+        void insertNewLine() {
+            stream << "\n";
+        }
+        
+        auto str() const {
+            return stream.str();
+        }
+    };
+    
+    OutputFileImpl::OutputFileImpl(Writer *writer) : writer{writer} {}
+
+    void OutputFileImpl::writeTrial(const av_coordinated_response_measure::Trial &trial) {
+        TrialStream stream;
+        stream.insert(trial.SNR_dB);
+        stream.insertCommaAndSpace();
+        stream.insert(trial.correctNumber);
+        stream.insertCommaAndSpace();
+        stream.insert(trial.subjectNumber);
+        stream.insertCommaAndSpace();
+        stream.insert(colorName(trial.correctColor));
+        stream.insertCommaAndSpace();
+        stream.insert(colorName(trial.subjectColor));
+        stream.insertCommaAndSpace();
+        stream.insert(trial.reversals);
+        stream.insertNewLine();
+        writer->write(stream.str());
+    }
+
+    std::string OutputFileImpl::colorName(av_coordinated_response_measure::Color c) {
+        switch (c) {
+        case av_coordinated_response_measure::Color::green:
+            return "green";
+        case av_coordinated_response_measure::Color::red:
+            return "red";
+        }
+    }
+}
--- src/recognition-test/OutputFileImpl.hpp
+++ src/recognition-test/OutputFileImpl.hpp
@@ -2,7 +2,6 @@
 #define recognition_test_OutputFileImpl_hpp
 
 #include "RecognitionTestModel.hpp"
-#include <sstream>
 
 namespace recognition_test {
     class Writer {
@@ -11,58 +10,13 @@ namespace recognition_test {
         virtual void write(std::string) = 0;
     };
 
-    class TrialStream {
-        std::stringstream stream;
-    public:
-        template<typename T>
-        void insert(T item) {
-            stream << item;
-        }
-        
-        void insertCommaAndSpace() {
-            stream << ", ";
-        }
-        
-        void insertNewLine() {
-            stream << "\n";
-        }
-        
-        auto str() const {
-            return stream.str();
-        }
-    };
-
     class OutputFileImpl : public OutputFile {
         Writer *writer;
     public:
-        OutputFileImpl(Writer *writer) : writer{writer} {}
-        
-        void writeTrial(const av_coordinated_response_measure::Trial &trial) {
-            TrialStream stream;
-            stream.insert(trial.SNR_dB);
-            stream.insertCommaAndSpace();
-            stream.insert(trial.correctNumber);
-            stream.insertCommaAndSpace();
-            stream.insert(trial.subjectNumber);
-            stream.insertCommaAndSpace();
-            stream.insert(colorName(trial.correctColor));
-            stream.insertCommaAndSpace();
-            stream.insert(colorName(trial.subjectColor));
-            stream.insertCommaAndSpace();
-            stream.insert(trial.reversals);
-            stream.insertNewLine();
-            writer->write(stream.str());
-        }
-        
+        OutputFileImpl(Writer *);
+        void writeTrial(const av_coordinated_response_measure::Trial &);
     private:
-        std::string colorName(av_coordinated_response_measure::Color c) {
-            switch (c) {
-            case av_coordinated_response_measure::Color::green:
-                return "green";
-            case av_coordinated_response_measure::Color::red:
-                return "red";
-            }
-        }
+        std::string colorName(av_coordinated_response_measure::Color);
     };
 }
 
