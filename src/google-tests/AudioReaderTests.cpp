c5ce9c536585edadc831e9cc0cc55ff521ece555
Seth Bashford <seth.bashford@boystown.org>
2019-04-09 11:52:36 -0500

    preparing for new tests

---
+++ src/google-tests/AudioReaderTests.cpp
@@ -0,0 +1,12 @@
+class AudioReader {
+
+};
+
+#include <gtest/gtest.h>
+
+namespace {
+    class AudioReaderTests : public ::testing::Test {
+    protected:
+        AudioReader reader{};
+    };
+}
