12a8860b1649403c5a45edc098a84847f83797b9
Seth Bashford <seth.bashford@boystown.org>
2019-04-15 16:09:45 -0500

    introducing into masker

---
+++ src/google-tests/AudioReaderStub.h
@@ -0,0 +1,12 @@
+//
+//  AudioReaderStub.h
+//  av-coordinate-response-measure
+//
+//  Created by Bashford, Seth on 4/15/19.
+//
+
+#ifndef AudioReaderStub_h
+#define AudioReaderStub_h
+
+
+#endif /* AudioReaderStub_h */
