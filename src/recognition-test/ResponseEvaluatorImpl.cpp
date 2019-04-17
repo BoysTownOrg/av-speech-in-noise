a1987f41daf411fae600e1049d0c3aefad8482d8
Seth Bashford <seth.bashford@boystown.org>
2019-04-17 09:36:41 -0500

    refactor

--- src/recognition-test/ResponseEvaluatorImpl.cpp
+++ src/recognition-test/ResponseEvaluatorImpl.cpp
@@ -1,8 +1,32 @@
-//
-//  ResponseEvaluatorImpl.cpp
-//  recognition-test
-//
-//  Created by Bashford, Seth on 4/17/19.
-//
-
 #include "ResponseEvaluatorImpl.hpp"
+
+namespace av_coordinate_response_measure {
+    bool ResponseEvaluatorImpl::correct(
+        std::string filePath,
+        const av_coordinate_response_measure::SubjectResponse &r
+    )  {
+        auto found = filePath.find(".");
+        auto correctNumber = std::stoi(filePath.substr(found-1, 1));
+        auto correctColor_ = correctColor(filePath);
+        return
+            correctNumber == r.number &&
+            correctColor_ == r.color;
+    }
+    
+    Color ResponseEvaluatorImpl::correctColor(std::string filePath) {
+        auto fileSeparator = filePath.find_last_of("/");
+        auto extension = filePath.find(".");
+        auto fileNameBeginning = fileSeparator+1;
+        auto beforeExtension = extension - 1;
+        auto fileNameLength = beforeExtension - fileNameBeginning;
+        auto colorName = filePath.substr(fileNameBeginning, fileNameLength);
+        if (colorName == "green")
+            return Color::green;
+        else if (colorName == "blue")
+            return Color::blue;
+        else if (colorName == "red")
+            return Color::red;
+        else
+            return Color::white;
+    }
+}
--- src/recognition-test/ResponseEvaluatorImpl.hpp
+++ src/recognition-test/ResponseEvaluatorImpl.hpp
@@ -4,38 +4,15 @@
 #include <recognition-test/RecognitionTestModel.hpp>
 
 namespace av_coordinate_response_measure {
-    class ResponseEvaluatorImpl :
-        public av_coordinate_response_measure::ResponseEvaluator
-    {
+    class ResponseEvaluatorImpl : public ResponseEvaluator {
     public:
         bool correct(
             std::string filePath,
-            const av_coordinate_response_measure::SubjectResponse &r
-        ) override {
-            auto found = filePath.find(".");
-            auto correctNumber = std::stoi(filePath.substr(found-1, 1));
-            auto correctColor_ = correctColor(filePath);
-            return
-                correctNumber == r.number &&
-                correctColor_ == r.color;
-        }
-        
-        av_coordinate_response_measure::Color correctColor(std::string filePath) {
-            auto fileSeparator = filePath.find_last_of("/");
-            auto extension = filePath.find(".");
-            auto fileNameBeginning = fileSeparator+1;
-            auto beforeExtension = extension - 1;
-            auto fileNameLength = beforeExtension - fileNameBeginning;
-            auto colorName = filePath.substr(fileNameBeginning, fileNameLength);
-            if (colorName == "green")
-                return av_coordinate_response_measure::Color::green;
-            else if (colorName == "blue")
-                return av_coordinate_response_measure::Color::blue;
-            else if (colorName == "red")
-                return av_coordinate_response_measure::Color::red;
-            else
-                return av_coordinate_response_measure::Color::white;
-        }
+            const SubjectResponse &r
+        ) override;
+    private:
+        Color correctColor(std::string filePath);
     };
 }
+
 #endif
