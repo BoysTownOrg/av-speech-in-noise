6a0de350bbec3091aa1df1a9f898bc41782ee3de
Seth Bashford <seth.bashford@boystown.org>
2019-04-17 13:59:16 -0500

    refactor

--- src/google-tests/AdaptiveTrackTests.cpp
+++ src/google-tests/AdaptiveTrackTests.cpp
@@ -22,27 +22,47 @@ namespace {
         void push(std::string directions) {
             for (const auto &c : directions)
                 if (c == 'd')
-                    track.pushDown();
+                    pushDown();
                 else if (c == 'u')
-                    track.pushUp();
+                    pushUp();
+        }
+        
+        void pushDown() {
+            track.pushDown();
+        }
+        
+        void pushUp() {
+            track.pushUp();
+        }
+        
+        auto x() {
+            return track.x();
+        }
+        
+        auto complete() {
+            return track.complete();
+        }
+        
+        auto reversals() {
+            return track.reversals();
         }
     };
 
     TEST_F(AdaptiveTrackTests, xEqualToStartingX) {
         settings.startingX = 1;
         reset();
-        EXPECT_EQ(1, track.x());
+        EXPECT_EQ(1, x());
     }
 
     TEST_F(AdaptiveTrackTests, noRunSequencesMeansNoStepChanges) {
         settings.startingX = 5;
         reset();
-        track.pushDown();
-        EXPECT_EQ(5, track.x());
-        track.pushDown();
-        EXPECT_EQ(5, track.x());
-        track.pushUp();
-        EXPECT_EQ(5, track.x());
+        pushDown();
+        EXPECT_EQ(5, x());
+        pushDown();
+        EXPECT_EQ(5, x());
+        pushUp();
+        EXPECT_EQ(5, x());
     }
 
     TEST_F(AdaptiveTrackTests, exhaustedRunSequencesMeansNoMoreStepChanges) {
@@ -52,22 +72,22 @@ namespace {
         rule.at(0).up = 1;
         rule.at(0).down = 2;
         reset();
-        track.pushDown();
-        EXPECT_EQ(5, track.x());
-        track.pushDown();
-        EXPECT_EQ(5 - 4, track.x());
-        track.pushUp();
-        EXPECT_EQ(5 - 4 + 4, track.x());
-        track.pushDown();
-        EXPECT_EQ(5 - 4 + 4, track.x());
-        track.pushDown();
-        EXPECT_EQ(5 - 4 + 4 - 4, track.x());
-        track.pushUp();
-        EXPECT_EQ(5 - 4 + 4 - 4, track.x());
-        track.pushDown();
-        EXPECT_EQ(5 - 4 + 4 - 4, track.x());
-        track.pushDown();
-        EXPECT_EQ(5 - 4 + 4 - 4, track.x());
+        pushDown();
+        EXPECT_EQ(5, x());
+        pushDown();
+        EXPECT_EQ(5 - 4, x());
+        pushUp();
+        EXPECT_EQ(5 - 4 + 4, x());
+        pushDown();
+        EXPECT_EQ(5 - 4 + 4, x());
+        pushDown();
+        EXPECT_EQ(5 - 4 + 4 - 4, x());
+        pushUp();
+        EXPECT_EQ(5 - 4 + 4 - 4, x());
+        pushDown();
+        EXPECT_EQ(5 - 4 + 4 - 4, x());
+        pushDown();
+        EXPECT_EQ(5 - 4 + 4 - 4, x());
     }
 
     TEST_F(AdaptiveTrackTests, completeWhenExhausted) {
@@ -77,19 +97,19 @@ namespace {
         rule.at(0).up = 1;
         rule.at(0).down = 2;
         reset();
-        EXPECT_FALSE(track.complete());
-        track.pushDown();
-        EXPECT_FALSE(track.complete());
-        track.pushDown();
-        EXPECT_FALSE(track.complete());
-        track.pushUp();
-        EXPECT_FALSE(track.complete());
-        track.pushDown();
-        EXPECT_FALSE(track.complete());
-        track.pushDown();
-        EXPECT_FALSE(track.complete());
-        track.pushUp();
-        EXPECT_TRUE(track.complete());
+        EXPECT_FALSE(complete());
+        pushDown();
+        EXPECT_FALSE(complete());
+        pushDown();
+        EXPECT_FALSE(complete());
+        pushUp();
+        EXPECT_FALSE(complete());
+        pushDown();
+        EXPECT_FALSE(complete());
+        pushDown();
+        EXPECT_FALSE(complete());
+        pushUp();
+        EXPECT_TRUE(complete());
     }
 
     // see https://doi.org/10.1121/1.1912375
@@ -101,7 +121,7 @@ namespace {
         rule.at(0).up = 1;
         reset();
         push("dduuuudduuuddddduuudduu");
-        EXPECT_EQ(1, track.x());
+        EXPECT_EQ(1, x());
     }
 
     // see https://doi.org/10.1121/1.1912375
@@ -113,7 +133,7 @@ namespace {
         rule.at(0).up = 1;
         reset();
         push("dddduduududdddduuuddddd");
-        EXPECT_EQ(1, track.x());
+        EXPECT_EQ(1, x());
     }
 
     TEST_F(AdaptiveTrackTests, twoSequences) {
@@ -127,28 +147,28 @@ namespace {
         rule.at(1).down = 2;
         rule.at(1).up = 1;
         reset();
-        track.pushDown();
-        EXPECT_EQ(65, track.x());
-        track.pushDown();
-        EXPECT_EQ(65 - 8, track.x());
-        track.pushDown();
-        EXPECT_EQ(65 - 8, track.x());
-        track.pushDown();
-        EXPECT_EQ(65 - 8 - 8, track.x());
-        track.pushUp();
-        EXPECT_EQ(65 - 8 - 8 + 8, track.x());
-        track.pushUp();
-        EXPECT_EQ(65 - 8 - 8 + 8 + 8, track.x());
-        track.pushDown();
-        EXPECT_EQ(65 - 8 - 8 + 8 + 8, track.x());
-        track.pushDown();
-        EXPECT_EQ(65 - 8 - 8 + 8 + 8 - 4, track.x());
-        track.pushDown();
-        EXPECT_EQ(65 - 8 - 8 + 8 + 8 - 4, track.x());
-        track.pushDown();
-        EXPECT_EQ(65 - 8 - 8 + 8 + 8 - 4 - 4, track.x());
-        track.pushUp();
-        EXPECT_EQ(65 - 8 - 8 + 8 + 8 - 4 - 4, track.x());
+        pushDown();
+        EXPECT_EQ(65, x());
+        pushDown();
+        EXPECT_EQ(65 - 8, x());
+        pushDown();
+        EXPECT_EQ(65 - 8, x());
+        pushDown();
+        EXPECT_EQ(65 - 8 - 8, x());
+        pushUp();
+        EXPECT_EQ(65 - 8 - 8 + 8, x());
+        pushUp();
+        EXPECT_EQ(65 - 8 - 8 + 8 + 8, x());
+        pushDown();
+        EXPECT_EQ(65 - 8 - 8 + 8 + 8, x());
+        pushDown();
+        EXPECT_EQ(65 - 8 - 8 + 8 + 8 - 4, x());
+        pushDown();
+        EXPECT_EQ(65 - 8 - 8 + 8 + 8 - 4, x());
+        pushDown();
+        EXPECT_EQ(65 - 8 - 8 + 8 + 8 - 4 - 4, x());
+        pushUp();
+        EXPECT_EQ(65 - 8 - 8 + 8 + 8 - 4 - 4, x());
     }
 
     TEST_F(AdaptiveTrackTests, threeSequences) {
@@ -167,7 +187,7 @@ namespace {
         rule.at(2).up = 1;
         reset();
         push("ddudddudddddudddddduddd");
-        EXPECT_EQ(3, track.x());
+        EXPECT_EQ(3, x());
     }
 
     TEST_F(AdaptiveTrackTests, varyingDownUpRule) {
@@ -181,28 +201,28 @@ namespace {
         rule.at(1).up = 2;
         rule.at(1).down = 1;
         reset();
-        track.pushDown();
-        EXPECT_EQ(65, track.x());
-        track.pushDown();
-        EXPECT_EQ(65 - 8, track.x());
-        track.pushDown();
-        EXPECT_EQ(65 - 8, track.x());
-        track.pushDown();
-        EXPECT_EQ(65 - 8 - 8, track.x());
-        track.pushUp();
-        EXPECT_EQ(65 - 8 - 8 + 8, track.x());
-        track.pushUp();
-        EXPECT_EQ(65 - 8 - 8 + 8 + 8, track.x());
-        track.pushDown();
-        EXPECT_EQ(65 - 8 - 8 + 8 + 8, track.x());
-        track.pushDown();
-        EXPECT_EQ(65 - 8 - 8 + 8 + 8 - 4, track.x());
-        track.pushDown();
-        EXPECT_EQ(65 - 8 - 8 + 8 + 8 - 4 - 4, track.x());
-        track.pushUp();
-        EXPECT_EQ(65 - 8 - 8 + 8 + 8 - 4 - 4, track.x());
-        track.pushUp();
-        EXPECT_EQ(65 - 8 - 8 + 8 + 8 - 4 - 4, track.x());
+        pushDown();
+        EXPECT_EQ(65, x());
+        pushDown();
+        EXPECT_EQ(65 - 8, x());
+        pushDown();
+        EXPECT_EQ(65 - 8, x());
+        pushDown();
+        EXPECT_EQ(65 - 8 - 8, x());
+        pushUp();
+        EXPECT_EQ(65 - 8 - 8 + 8, x());
+        pushUp();
+        EXPECT_EQ(65 - 8 - 8 + 8 + 8, x());
+        pushDown();
+        EXPECT_EQ(65 - 8 - 8 + 8 + 8, x());
+        pushDown();
+        EXPECT_EQ(65 - 8 - 8 + 8 + 8 - 4, x());
+        pushDown();
+        EXPECT_EQ(65 - 8 - 8 + 8 + 8 - 4 - 4, x());
+        pushUp();
+        EXPECT_EQ(65 - 8 - 8 + 8 + 8 - 4 - 4, x());
+        pushUp();
+        EXPECT_EQ(65 - 8 - 8 + 8 + 8 - 4 - 4, x());
     }
     
     TEST_F(AdaptiveTrackTests, reversals) {
@@ -211,18 +231,18 @@ namespace {
         rule.at(0).down = 2;
         rule.at(0).up = 1;
         reset();
-        EXPECT_EQ(int{0}, track.reversals());
-        track.pushUp();
-        EXPECT_EQ(int{0}, track.reversals());
-        track.pushDown();
-        EXPECT_EQ(int{0}, track.reversals());
-        track.pushDown();
-        EXPECT_EQ(1, track.reversals());
-        track.pushUp();
-        EXPECT_EQ(2, track.reversals());
-        track.pushDown();
-        EXPECT_EQ(2, track.reversals());
-        track.pushDown();
-        EXPECT_EQ(3, track.reversals());
+        EXPECT_EQ(int{0}, reversals());
+        pushUp();
+        EXPECT_EQ(int{0}, reversals());
+        pushDown();
+        EXPECT_EQ(int{0}, reversals());
+        pushDown();
+        EXPECT_EQ(1, reversals());
+        pushUp();
+        EXPECT_EQ(2, reversals());
+        pushDown();
+        EXPECT_EQ(2, reversals());
+        pushDown();
+        EXPECT_EQ(3, reversals());
     }
 }
