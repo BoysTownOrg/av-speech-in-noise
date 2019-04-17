d8bd2f477e40fbf0b49823ac54231b7a74e337e3
Seth Bashford <seth.bashford@boystown.org>
2019-04-17 09:33:33 -0500

    refactor

--- src/google-tests/ResponseEvaluatorTests.cpp
+++ src/google-tests/ResponseEvaluatorTests.cpp
@@ -1,48 +1,13 @@
-#include <recognition-test/RecognitionTestModel.hpp>
-
-class ResponseEvaluatorImpl :
-    public av_coordinate_response_measure::ResponseEvaluator
-{
-public:
-    bool correct(
-        std::string filePath,
-        const av_coordinate_response_measure::SubjectResponse &r
-    ) override {
-        auto found = filePath.find(".");
-        auto correctNumber = std::stoi(filePath.substr(found-1, 1));
-        auto correctColor_ = correctColor(filePath);
-        return
-            correctNumber == r.number &&
-            correctColor_ == r.color;
-    }
-    
-    av_coordinate_response_measure::Color correctColor(std::string filePath) {
-        auto fileSeparator = filePath.find_last_of("/");
-        auto extension = filePath.find(".");
-        auto fileNameBeginning = fileSeparator+1;
-        auto beforeExtension = extension - 1;
-        auto fileNameLength = beforeExtension - fileNameBeginning;
-        auto colorName = filePath.substr(fileNameBeginning, fileNameLength);
-        if (colorName == "green")
-            return av_coordinate_response_measure::Color::green;
-        else if (colorName == "blue")
-            return av_coordinate_response_measure::Color::blue;
-        else if (colorName == "red")
-            return av_coordinate_response_measure::Color::red;
-        else
-            return av_coordinate_response_measure::Color::white;
-    }
-};
-
+#include <recognition-test/ResponseEvaluatorImpl.hpp>
 #include <gtest/gtest.h>
 
 namespace {
     class ResponseEvaluatorTests : public ::testing::Test {
     protected:
-        ResponseEvaluatorImpl evaluator{};
+        av_coordinate_response_measure::ResponseEvaluatorImpl evaluator{};
     };
     
-    TEST_F(ResponseEvaluatorTests, tbd) {
+    TEST_F(ResponseEvaluatorTests, blue) {
         EXPECT_TRUE(evaluator.correct(
             "blue1.mov",
             {
@@ -52,7 +17,7 @@ namespace {
         ));
     }
     
-    TEST_F(ResponseEvaluatorTests, tbd2) {
+    TEST_F(ResponseEvaluatorTests, red) {
         EXPECT_TRUE(evaluator.correct(
             "red1.mov",
             {
@@ -62,12 +27,22 @@ namespace {
         ));
     }
     
-    TEST_F(ResponseEvaluatorTests, tbd3) {
+    TEST_F(ResponseEvaluatorTests, white) {
         EXPECT_TRUE(evaluator.correct(
             "white1.mov",
             {
                 1,
                 av_coordinate_response_measure::Color::white
+            }
+        ));
+    }
+    
+    TEST_F(ResponseEvaluatorTests, green) {
+        EXPECT_TRUE(evaluator.correct(
+            "green1.mov",
+            {
+                1,
+                av_coordinate_response_measure::Color::green
             }
         ));
     }
---
+++ src/recognition-test/ResponseEvaluatorImpl.cpp
@@ -0,0 +1,8 @@
+//
+//  ResponseEvaluatorImpl.cpp
+//  recognition-test
+//
+//  Created by Bashford, Seth on 4/17/19.
+//
+
+#include "ResponseEvaluatorImpl.hpp"
