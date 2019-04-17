f2aa2396885f0131c28336ae8ad16dc39853384f
Seth Bashford <seth.bashford@boystown.org>
2019-04-17 09:16:24 -0500

    add test

--- src/google-tests/ResponseEvaluatorTests.cpp
+++ src/google-tests/ResponseEvaluatorTests.cpp
@@ -6,9 +6,11 @@ class ResponseEvaluatorImpl :
 public:
     bool correct(
         std::string filePath,
-        const av_coordinate_response_measure::SubjectResponse &
+        const av_coordinate_response_measure::SubjectResponse &r
     ) override {
-        return true;
+        auto found = filePath.find(".");
+        auto correctNumber = std::stoi(filePath.substr(found-1, 1));
+        return correctNumber == r.number;
     }
 };
 
@@ -23,6 +25,16 @@ namespace {
     TEST_F(ResponseEvaluatorTests, tbd) {
         EXPECT_TRUE(evaluator.correct(
             "blue1.mov",
+            {
+                1,
+                av_coordinate_response_measure::Color::blue
+            }
+        ));
+    }
+    
+    TEST_F(ResponseEvaluatorTests, rightColorWrongNumber) {
+        EXPECT_FALSE(evaluator.correct(
+            "blue2.mov",
             {
                 1,
                 av_coordinate_response_measure::Color::blue
