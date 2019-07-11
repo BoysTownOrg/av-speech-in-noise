9ac39193c696222e4658ddb2949ef877ed3bed7a
Seth Bashford <seth.bashford@boystown.org>
2019-07-11 15:53:53 -0500

    smaller video window

--- src/main/AvFoundationPlayers.mm
+++ src/main/AvFoundationPlayers.mm
@@ -407,8 +407,11 @@ void AvFoundationVideoPlayer::prepareVideo() {
 
 void AvFoundationVideoPlayer::resizeVideo() {
     AvAssetFacade asset{player.currentItem.asset};
+    auto size = asset.videoTrack().naturalSize;
+    size.height /= 2;
+    size.width /= 2;
     [videoWindow setContentSize:
-        NSSizeFromCGSize(asset.videoTrack().naturalSize)
+        NSSizeFromCGSize(size)
     ];
     [playerLayer setFrame:videoWindow.contentView.bounds];
 }
