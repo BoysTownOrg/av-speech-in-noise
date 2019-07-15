1d5f21f2557983890e006a93c04e1290e51d1531
Seth Bashford <seth.bashford@boystown.org>
2019-07-15 13:37:40 -0500

    refactor

--- src/google-tests/RecognitionTestModelTests.cpp
+++ src/google-tests/RecognitionTestModelTests.cpp
@@ -4,7 +4,6 @@
 #include <gtest/gtest.h>
 
 namespace av_speech_in_noise::tests::recognition_test {
-    
     class AdaptiveMethodStub : public IAdaptiveMethod {
         const AdaptiveTest *test_{};
     public:
