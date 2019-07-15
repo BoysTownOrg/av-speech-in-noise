db94583049a23fa2cdd3810b9556a6a89a4f95cb
Seth Bashford <seth.bashford@boystown.org>
2019-07-15 11:06:10 -0500

    refactor

--- src/google-tests/AdaptiveMethodTests.cpp
+++ src/google-tests/AdaptiveMethodTests.cpp
@@ -20,47 +20,44 @@ namespace av_speech_in_noise::tests {
             &evaluator,
             &randomizer
         };
+        AdaptiveTest test;
         TrackingRule targetLevelRule_;
+        std::vector<std::shared_ptr<TargetList>> lists;
+        std::vector<std::shared_ptr<Track>> tracks;
+        
+        AdaptiveMethodTests() {
+            test.targetLevelRule = &targetLevelRule_;
+            for (int i = 0; i < 3; ++i) {
+                lists.push_back(std::make_shared<TargetListStub>());
+                tracks.push_back(std::make_shared<TrackStub>());
+            }
+            targetListSetReader.setTargetLists(lists);
+            snrTrackFactory.setTracks(tracks);
+        }
         
         void assertSettingsContainTargetLevelRule(const Track::Settings &s) {
             const auto *rule = &targetLevelRule_;
             assertEqual(rule, s.rule);
         }
+        
+        void initialize() {
+            method.initialize(test);
+        }
     };
     
     TEST_F(
         AdaptiveMethodTests,
-        loadFromDirectoryCreatesSnrTrackForEachList
+        initializeCreatesSnrTrackForEachList
     ) {
-        std::vector<std::shared_ptr<TargetList>> lists;
-        std::vector<std::shared_ptr<Track>> tracks;
-        for (int i = 0; i < 3; ++i) {
-            lists.push_back(std::make_shared<TargetListStub>());
-            tracks.push_back(std::make_shared<TrackStub>());
-        }
-        targetListSetReader.setTargetLists(lists);
-        snrTrackFactory.setTracks(tracks);
-        AdaptiveTest test;
-        test.targetLevelRule = &targetLevelRule_;
-        method.initialize(test);
+        initialize();
         assertEqual(3UL, snrTrackFactory.parameters().size());
     }
 
     TEST_F(
         AdaptiveMethodTests,
-        loadFromDirectoryCreatesEachSnrTrackWithTargetLevelRule
+        initializeCreatesEachSnrTrackWithTargetLevelRule
     ) {
-        std::vector<std::shared_ptr<TargetList>> lists;
-        std::vector<std::shared_ptr<Track>> tracks;
-        for (int i = 0; i < 3; ++i) {
-            lists.push_back(std::make_shared<TargetListStub>());
-            tracks.push_back(std::make_shared<TrackStub>());
-        }
-        targetListSetReader.setTargetLists(lists);
-        snrTrackFactory.setTracks(tracks);
-        AdaptiveTest test;
-        test.targetLevelRule = &targetLevelRule_;
-        method.initialize(test);
+        initialize();
         for (int i = 0; i < 3; ++i)
             assertSettingsContainTargetLevelRule(
                 snrTrackFactory.parameters().at(i)
