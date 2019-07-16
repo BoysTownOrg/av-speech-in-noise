692c833e77ca7f51b4033a8a5ef9c2543dcd9da9
Seth Bashford <seth.bashford@boystown.org>
2019-07-16 16:54:42 -0500

    add test

--- src/google-tests/TrackSettingsReaderTests.cpp
+++ src/google-tests/TrackSettingsReaderTests.cpp
@@ -6,15 +6,28 @@ namespace av_speech_in_noise {
         virtual ~TextFileReader() = default;
         virtual std::string read(std::string) = 0;
     };
+    
+    class ITrackSettingsInterpreter {
+    public:
+        virtual ~ITrackSettingsInterpreter() = default;
+        virtual TrackingRule trackingRule(std::string) = 0;
+    };
 
     class TrackSettingsReader : public ITrackSettingsReader {
         TextFileReader *reader;
+        ITrackSettingsInterpreter *interpreter;
     public:
-        explicit TrackSettingsReader(TextFileReader *reader) :
-            reader{reader} {}
+        explicit TrackSettingsReader(
+            TextFileReader *reader,
+            ITrackSettingsInterpreter *interpreter = {}
+        ) :
+            reader{reader},
+            interpreter{interpreter} {}
         
         const TrackingRule *read(std::string filePath) override {
-            reader->read(std::move(filePath));
+            auto contents = reader->read(std::move(filePath));
+            if (interpreter)
+                interpreter->trackingRule(contents);
             return {};
         }
     };
@@ -26,6 +39,7 @@ namespace av_speech_in_noise {
 namespace av_speech_in_noise::tests {
     class TextFileReaderStub : public TextFileReader {
         std::string filePath_{};
+        std::string contents_{};
     public:
         auto filePath() const {
             return filePath_;
@@ -33,7 +47,24 @@ namespace av_speech_in_noise::tests {
         
         std::string read(std::string s) override {
             filePath_ = std::move(s);
+            return contents_;
+        }
+        
+        void setContents(std::string s) {
+            contents_ = std::move(s);
+        }
+    };
+    
+    class TrackSettingsInterpreterStub : public ITrackSettingsInterpreter {
+        std::string content_{};
+    public:
+        TrackingRule trackingRule(std::string s) override {
+            content_ = std::move(s);
             return {};
+        }
+        
+        auto content() const {
+            return content_;
         }
     };
     
@@ -41,10 +72,19 @@ namespace av_speech_in_noise::tests {
     
     };
     
-    TEST_F(TrackSettingsReaderTests, tbd) {
+    TEST_F(TrackSettingsReaderTests, readPassesFilePathToFileReader) {
         TextFileReaderStub fileReader;
         TrackSettingsReader reader{&fileReader};
         reader.read("a");
         assertEqual("a", fileReader.filePath());
+    }
+    
+    TEST_F(TrackSettingsReaderTests, readPassesContentsToInterpreter) {
+        TextFileReaderStub fileReader;
+        TrackSettingsInterpreterStub interpreter;
+        TrackSettingsReader reader{&fileReader, &interpreter};
+        fileReader.setContents("a");
+        reader.read({});
+        assertEqual("a", interpreter.content());
     }
 }
