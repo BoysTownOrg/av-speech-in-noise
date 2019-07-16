25206df2657f61cf89778d95666b60132aa9df23
Seth Bashford <seth.bashford@boystown.org>
2019-07-16 13:10:40 -0500

    refactor

--- src/recognition-test/TrackSettingsReader.cpp
+++ src/recognition-test/TrackSettingsReader.cpp
@@ -1,8 +1,65 @@
-//
-//  TrackSettingsReader.cpp
-//  recognition-test
-//
-//  Created by Bashford, Seth on 7/16/19.
-//
-
 #include "TrackSettingsReader.hpp"
+
+namespace av_speech_in_noise {
+    namespace {
+        class Stream {
+            std::stringstream parent;
+            std::stringstream line_{};
+            std::string lastLine_{};
+            bool failed_{};
+        public:
+            explicit Stream(std::string s) : parent{std::move(s)} {}
+            
+            void nextLine() {
+                std::getline(parent, lastLine_);
+                resetLine_();
+            }
+            
+            void resetLine() {
+                resetLine_();
+            }
+            
+            std::string propertyName() {
+                return lastLine_.substr(0, findPropertyNameDelimiter());
+            }
+            
+            void resetLine_() {
+                line_ = std::stringstream{lastLine_.substr(findPropertyNameDelimiter()+1)};
+            }
+            
+            std::string::size_type findPropertyNameDelimiter() {
+                return lastLine_.find(':');
+            }
+            
+            int value() {
+                int x;
+                if (!(line_ >> x))
+                    failed_ = true;
+                return x;
+            }
+            
+            auto failed() const {
+                return failed_;
+            }
+        };
+    }
+    
+    TrackingRule TrackSettingsReader::trackingRule() {
+        auto stream_ = Stream{contents};
+        stream_.nextLine();
+        TrackingRule rule;
+        stream_.value();
+        while (!stream_.failed()) {
+            rule.push_back({});
+            stream_.value();
+        }
+        stream_.resetLine();
+        for (int i = 0; i < 4; ++i) {
+            auto f = propertyApplication.at(stream_.propertyName());
+            for (auto &sequence : rule)
+                (this->*f)(sequence, stream_.value());
+            stream_.nextLine();
+        }
+        return rule;
+    }
+}
--- src/recognition-test/TrackSettingsReader.hpp
+++ src/recognition-test/TrackSettingsReader.hpp
@@ -7,48 +7,6 @@
 #include <map>
 
 namespace av_speech_in_noise {
-    namespace {
-        class Stream {
-            std::stringstream parent;
-            std::stringstream line_{};
-            std::string lastLine_{};
-            bool failed_{};
-        public:
-            explicit Stream(std::string s) : parent{std::move(s)} {}
-            
-            void nextLine() {
-                std::getline(parent, lastLine_);
-                resetLine_();
-            }
-            
-            void resetLine() {
-                resetLine_();
-            }
-            
-            std::string propertyName() {
-                return lastLine_.substr(0, findPropertyNameDelimiter());
-            }
-            
-            void resetLine_() {
-                line_ = std::stringstream{lastLine_.substr(findPropertyNameDelimiter()+1)};
-            }
-            
-            std::string::size_type findPropertyNameDelimiter() {
-                return lastLine_.find(':');
-            }
-            
-            int value() {
-                int x;
-                if (!(line_ >> x))
-                    failed_ = true;
-                return x;
-            }
-            
-            auto failed() const {
-                return failed_;
-            }
-        };
-    }
     
     enum class TrackProperty {
         up,
@@ -83,24 +41,7 @@ namespace av_speech_in_noise {
         explicit TrackSettingsReader(std::string s) :
             contents{std::move(s)} {}
         
-        TrackingRule trackingRule() {
-            auto stream_ = Stream{contents};
-            stream_.nextLine();
-            TrackingRule rule;
-            stream_.value();
-            while (!stream_.failed()) {
-                rule.push_back({});
-                stream_.value();
-            }
-            stream_.resetLine();
-            for (int i = 0; i < 4; ++i) {
-                auto f = propertyApplication.at(stream_.propertyName());
-                for (auto &sequence : rule)
-                    (this->*f)(sequence, stream_.value());
-                stream_.nextLine();
-            }
-            return rule;
-        }
+        TrackingRule trackingRule();
         
         void applyToUp(TrackingSequence &sequence, int x) {
             sequence.up = x;
