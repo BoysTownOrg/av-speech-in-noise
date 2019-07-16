c1bfbb3312820bcf8c957accb131a3b98a6ad9dc
Seth Bashford <seth.bashford@boystown.org>
2019-07-16 10:34:51 -0500

    add test

--- src/google-tests/TrackSettingsReaderTests.cpp
+++ src/google-tests/TrackSettingsReaderTests.cpp
@@ -10,27 +10,53 @@ namespace av_speech_in_noise {
         
         TrackingRule trackingRule() {
             std::stringstream stream{contents};
+            std::stringstream line_;
             std::string ignore;
+            std::string line;
             int value;
-            stream >> ignore;
-            stream >> value;
+            int value2;
+            bool twoSequences{};
             TrackingSequence first;
+            TrackingSequence second;
+            std::getline(stream, line);
+            line_ = std::stringstream{line};
+            line_ >> ignore;
+            line_ >> value;
+            if (line_ >> value2) {
+                second.up = value2;
+                twoSequences = true;
+            }
             first.up = value;
-            stream >> ignore;
-            stream >> value;
+            std::getline(stream, line);
+            line_ = std::stringstream{line};
+            line_ >> ignore;
+            line_ >> value;
+            if (line_ >> value2)
+                second.down = value2;
             first.down = value;
-            stream >> ignore;
-            stream >> ignore;
-            stream >> ignore;
-            stream >> ignore;
-            stream >> value;
+            std::getline(stream, line);
+            line_ = std::stringstream{line};
+            line_ >> ignore;
+            line_ >> ignore;
+            line_ >> ignore;
+            line_ >> ignore;
+            line_ >> value;
+            if (line_ >> value2)
+                second.runCount = value2;
             first.runCount = value;
-            stream >> ignore;
-            stream >> ignore;
-            stream >> ignore;
-            stream >> value;
+            std::getline(stream, line);
+            line_ = std::stringstream{line};
+            line_ >> ignore;
+            line_ >> ignore;
+            line_ >> ignore;
+            line_ >> value;
+            if (line_ >> value2)
+                second.stepSize = value2;
             first.stepSize = value;
-            return {first};
+            if (twoSequences)
+                return {first, second};
+            else
+                return {first};
         }
     };
 }
@@ -60,8 +86,28 @@ namespace av_speech_in_noise::tests {
             "up: 1\n"
             "down: 2\n"
             "reversals per step size: 3\n"
-            "step sizes (dB): 4\n",
+            "step sizes (dB): 4",
             {first}
+        );
+    }
+    
+    TEST_F(TrackSettingsReaderTests, twoSequences) {
+        TrackingSequence first;
+        first.up = 1;
+        first.down = 3;
+        first.runCount = 5;
+        first.stepSize = 7;
+        TrackingSequence second;
+        second.up = 2;
+        second.down = 4;
+        second.runCount = 6;
+        second.stepSize = 8;
+        assertFileContentsYield(
+            "up: 1 2\n"
+            "down: 3 4\n"
+            "reversals per step size: 5 6\n"
+            "step sizes (dB): 7 8",
+            {first, second}
         );
     }
 }
