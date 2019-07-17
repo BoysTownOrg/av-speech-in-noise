bf836dc1085581c3fbae76f6ea41d9fba12ac3cc
Seth Bashford <seth.bashford@boystown.org>
2019-07-17 09:15:51 -0500

    refactor

--- src/google-tests/TrackSettingsReaderTests.cpp
+++ src/google-tests/TrackSettingsReaderTests.cpp
@@ -1,36 +1,5 @@
-#include <recognition-test/AdaptiveMethod.hpp>
-
-namespace av_speech_in_noise {
-    class TextFileReader {
-    public:
-        virtual ~TextFileReader() = default;
-        virtual std::string read(std::string) = 0;
-    };
-    
-    class ITrackSettingsInterpreter {
-    public:
-        virtual ~ITrackSettingsInterpreter() = default;
-        virtual const TrackingRule *trackingRule(std::string) = 0;
-    };
-
-    class TrackSettingsReader : public ITrackSettingsReader {
-        TextFileReader *reader;
-        ITrackSettingsInterpreter *interpreter;
-    public:
-        TrackSettingsReader(
-            TextFileReader *reader,
-            ITrackSettingsInterpreter *interpreter
-        ) :
-            reader{reader},
-            interpreter{interpreter} {}
-        
-        const TrackingRule *read(std::string filePath) override {
-            return interpreter->trackingRule(reader->read(std::move(filePath)));
-        }
-    };
-}
-
 #include "assert-utility.h"
+#include <recognition-test/TrackSettingsReader.hpp>
 #include <gtest/gtest.h>
 
 namespace av_speech_in_noise::tests {
---
+++ src/recognition-test/TrackSettingsReader.cpp
@@ -0,0 +1,8 @@
+//
+//  TrackSettingsReader.cpp
+//  recognition-test
+//
+//  Created by Bashford, Seth on 7/17/19.
+//
+
+#include "TrackSettingsReader.hpp"
