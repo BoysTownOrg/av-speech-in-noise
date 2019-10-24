45ddc7f309a69a65499cb59d8b1528d188d3489a
Seth Bashford <seth.bashford@boystown.org>
2019-10-24 12:27:18 -0500

    same

--- av-speech-in-noise/recognition-test/include/recognition-test/ResponseEvaluatorImpl.hpp
+++ av-speech-in-noise/recognition-test/include/recognition-test/ResponseEvaluatorImpl.hpp
@@ -1,25 +1,25 @@
-#ifndef av_speech_in_noise_ResponseEvaluatorImpl_hpp
-#define av_speech_in_noise_ResponseEvaluatorImpl_hpp
+#ifndef AV_SPEECH_IN_NOISE_RECOGNITION_TEST_INCLUDE_RECOGNITION_TEST_RESPONSEEVALUATORIMPL_HPP_
+#define AV_SPEECH_IN_NOISE_RECOGNITION_TEST_INCLUDE_RECOGNITION_TEST_RESPONSEEVALUATORIMPL_HPP_
 
 #include <recognition-test/RecognitionTestModel.hpp>
 
 namespace av_speech_in_noise {
-    class ResponseEvaluatorImpl : public ResponseEvaluator {
-    public:
-        bool correct(
-            const std::string &filePath,
-            const coordinate_response_measure::SubjectResponse &r) override;
-        static int invalidNumber;
-        int correctNumber(const std::string &filePath) override;
-        coordinate_response_measure::Color correctColor(const std::string &filePath) override;
-        std::string fileName(const std::string &filePath) override;
-        
-    private:
-        coordinate_response_measure::Color color(const std::string &colorName);
-        unsigned long leadingPathLength(const std::string &filePath);
-        int correctNumber_(const std::string &filePath);
-        long colorNameLength(const std::string &filePath, unsigned long leadingPathLength_);
-    };
+class ResponseEvaluatorImpl : public ResponseEvaluator {
+public:
+    bool correct(
+        const std::string &filePath,
+        const coordinate_response_measure::SubjectResponse &r) override;
+    static int invalidNumber;
+    int correctNumber(const std::string &filePath) override;
+    coordinate_response_measure::Color correctColor(const std::string &filePath) override;
+    std::string fileName(const std::string &filePath) override;
+
+private:
+    coordinate_response_measure::Color color(const std::string &colorName);
+    unsigned long leadingPathLength(const std::string &filePath);
+    int correctNumber_(const std::string &filePath);
+    long colorNameLength(const std::string &filePath, unsigned long leadingPathLength_);
+};
 }
 
 #endif
--- av-speech-in-noise/recognition-test/include/recognition-test/TrackSettingsReader.hpp
+++ av-speech-in-noise/recognition-test/include/recognition-test/TrackSettingsReader.hpp
@@ -1,31 +1,32 @@
-#ifndef av_speech_in_noise_TrackSettingsReader_hpp
-#define av_speech_in_noise_TrackSettingsReader_hpp
+#ifndef AV_SPEECH_IN_NOISE_RECOGNITION_TEST_INCLUDE_RECOGNITION_TEST_TRACKSETTINGSREADER_HPP_
+#define AV_SPEECH_IN_NOISE_RECOGNITION_TEST_INCLUDE_RECOGNITION_TEST_TRACKSETTINGSREADER_HPP_
 
 #include "AdaptiveMethod.hpp"
+#include <string>
 
 namespace av_speech_in_noise {
-    class TextFileReader {
-    public:
-        virtual ~TextFileReader() = default;
-        virtual std::string read(std::string) = 0;
-    };
-    
-    class ITrackSettingsInterpreter {
-    public:
-        virtual ~ITrackSettingsInterpreter() = default;
-        virtual const TrackingRule *trackingRule(std::string) = 0;
-    };
+class TextFileReader {
+public:
+    virtual ~TextFileReader() = default;
+    virtual std::string read(std::string) = 0;
+};
 
-    class TrackSettingsReader : public ITrackSettingsReader {
-        TextFileReader *reader;
-        ITrackSettingsInterpreter *interpreter;
-    public:
-        TrackSettingsReader(
-            TextFileReader *,
-            ITrackSettingsInterpreter *
-        );
-        const TrackingRule *read(std::string filePath) override;
-    };
+class ITrackSettingsInterpreter {
+public:
+    virtual ~ITrackSettingsInterpreter() = default;
+    virtual const TrackingRule *trackingRule(std::string) = 0;
+};
+
+class TrackSettingsReader : public ITrackSettingsReader {
+    TextFileReader *reader;
+    ITrackSettingsInterpreter *interpreter;
+public:
+    TrackSettingsReader(
+        TextFileReader *,
+        ITrackSettingsInterpreter *
+    );
+    const TrackingRule *read(std::string filePath) override;
+};
 }
 
 #endif
