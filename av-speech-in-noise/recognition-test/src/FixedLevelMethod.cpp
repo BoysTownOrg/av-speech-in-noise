f9259c2b6b0e81d03444e81bbf9edb039746a8bf
Seth Bashford <seth.bashford@boystown.org>
2019-10-24 12:30:28 -0500

    same

--- av-speech-in-noise/recognition-test/src/FixedLevelMethod.cpp
+++ av-speech-in-noise/recognition-test/src/FixedLevelMethod.cpp
@@ -1,72 +1,66 @@
 #include "FixedLevelMethod.hpp"
 
 namespace av_speech_in_noise {
-    FixedLevelMethod::FixedLevelMethod(
-        ResponseEvaluator *evaluator
-    ) :
-        evaluator{evaluator} {}
-    
-    void FixedLevelMethod::initialize(
-        const FixedLevelTest &p, 
-        TargetList *list,
-        TestConcluder *concluder_
-    ) {
-        concluder = concluder_;
-        targetList = list;
-        test = &p;
-        snr_dB_ = p.snr_dB;
-        targetList->loadFromDirectory(p.common.targetListDirectory);
-        concluder->initialize(p);
-    }
-    
-    bool FixedLevelMethod::complete() {
-        return concluder->complete(targetList);
-    }
-    
-    std::string FixedLevelMethod::next() {
-        return targetList->next();
-    }
-    
-    int FixedLevelMethod::snr_dB() {
-        return snr_dB_;
-    }
-    
-    void FixedLevelMethod::submitResponse(
-        const coordinate_response_measure::SubjectResponse &response
-    ) {
-        auto current_ = current();
-        coordinate_response_measure::Trial trial;
-        trial.subjectColor = response.color;
-        trial.subjectNumber = response.number;
-        trial.correctColor = evaluator->correctColor(current_);
-        trial.correctNumber = evaluator->correctNumber(current_);
-        trial.correct = evaluator->correct(current_, response);
-        trial.stimulus = current_;
-        lastTrial.trial = trial;
-        concluder->submitResponse();
-    }
-    
-    std::string FixedLevelMethod::current() {
-        return targetList->current();
-    }
-    
-    void FixedLevelMethod::writeTestingParameters(OutputFile *file) {
-        file->writeTest(*test);
-    }
-    
-    void FixedLevelMethod::writeLastCoordinateResponse(OutputFile *file) {
-        file->writeTrial(lastTrial);
-    }
-    
-    void FixedLevelMethod::submitIncorrectResponse() {
-        
-    }
-    
-    void FixedLevelMethod::submitCorrectResponse() {
-        
-    }
-    
-    void FixedLevelMethod::submitResponse(const FreeResponse &) {
-    
-    }
+FixedLevelMethod::FixedLevelMethod(
+    ResponseEvaluator *evaluator
+) :
+    evaluator{evaluator} {}
+
+void FixedLevelMethod::initialize(
+    const FixedLevelTest &p,
+    TargetList *list,
+    TestConcluder *concluder_
+) {
+    concluder = concluder_;
+    targetList = list;
+    test = &p;
+    snr_dB_ = p.snr_dB;
+    targetList->loadFromDirectory(p.common.targetListDirectory);
+    concluder->initialize(p);
+}
+
+bool FixedLevelMethod::complete() {
+    return concluder->complete(targetList);
+}
+
+std::string FixedLevelMethod::next() {
+    return targetList->next();
+}
+
+int FixedLevelMethod::snr_dB() {
+    return snr_dB_;
+}
+
+void FixedLevelMethod::submitResponse(
+    const coordinate_response_measure::SubjectResponse &response
+) {
+    auto current_ = current();
+    coordinate_response_measure::Trial trial;
+    trial.subjectColor = response.color;
+    trial.subjectNumber = response.number;
+    trial.correctColor = evaluator->correctColor(current_);
+    trial.correctNumber = evaluator->correctNumber(current_);
+    trial.correct = evaluator->correct(current_, response);
+    trial.stimulus = current_;
+    lastTrial.trial = trial;
+    concluder->submitResponse();
+}
+
+std::string FixedLevelMethod::current() {
+    return targetList->current();
+}
+
+void FixedLevelMethod::writeTestingParameters(OutputFile *file) {
+    file->writeTest(*test);
+}
+
+void FixedLevelMethod::writeLastCoordinateResponse(OutputFile *file) {
+    file->writeTrial(lastTrial);
+}
+
+void FixedLevelMethod::submitIncorrectResponse() {}
+
+void FixedLevelMethod::submitCorrectResponse() {}
+
+void FixedLevelMethod::submitResponse(const FreeResponse &) {}
 }
--- av-speech-in-noise/recognition-test/src/TrackSettingsInterpreter.cpp
+++ av-speech-in-noise/recognition-test/src/TrackSettingsInterpreter.cpp
@@ -2,97 +2,97 @@
 #include <sstream>
 
 namespace av_speech_in_noise {
-    namespace {
-        class Stream {
-            std::stringstream stream;
-            std::stringstream line_{};
-            std::string lastLine_{};
-            bool failed_{};
-        public:
-            explicit Stream(std::string s) : stream{std::move(s)} {}
-            
-            bool nextLine() {
-                if (!std::getline(stream, lastLine_))
-                    return false;
-                resetLine_();
-                return true;
-            }
-            
-            std::string propertyName() {
-                return lastLine_.substr(0, findPropertyNameDelimiter());
-            }
-            
-            void resetLine_() {
-                line_ = std::stringstream{
-                    lastLine_.substr(findPropertyNameDelimiter()+1)
-                };
-                failed_ = false;
-            }
-            
-            std::string::size_type findPropertyNameDelimiter() {
-                return lastLine_.find(':');
-            }
-            
-            int value() {
-                int x;
-                if (!(line_ >> x))
-                    failed_ = true;
-                return x;
-            }
-            
-            auto failed() const {
-                return failed_;
-            }
+namespace {
+class Stream {
+    std::stringstream stream;
+    std::stringstream line_{};
+    std::string lastLine_{};
+    bool failed_{};
+public:
+    explicit Stream(std::string s) : stream{std::move(s)} {}
+
+    bool nextLine() {
+        if (!std::getline(stream, lastLine_))
+            return false;
+        resetLine_();
+        return true;
+    }
+
+    std::string propertyName() {
+        return lastLine_.substr(0, findPropertyNameDelimiter());
+    }
+
+    void resetLine_() {
+        line_ = std::stringstream{
+            lastLine_.substr(findPropertyNameDelimiter()+1)
         };
-    
-        void applyToUp(TrackingSequence &sequence, int x) {
-            sequence.up = x;
-        }
+        failed_ = false;
+    }
 
-        void applyToDown(TrackingSequence &sequence, int x) {
-            sequence.down = x;
-        }
+    std::string::size_type findPropertyNameDelimiter() {
+        return lastLine_.find(':');
+    }
 
-        void applyToRunCount(TrackingSequence &sequence, int x) {
-            sequence.runCount = x;
-        }
+    int value() {
+        int x;
+        if (!(line_ >> x))
+            failed_ = true;
+        return x;
+    }
 
-        void applyToStepSize(TrackingSequence &sequence, int x) {
-            sequence.stepSize = x;
-        }
+    auto failed() const {
+        return failed_;
+    }
+};
+}
 
-        void nothing(TrackingSequence &, int) {}
-        
-        void(*propertyApplication(const std::string &s))(TrackingSequence &, int) {
-            using Property = TrackSettingsInterpreter::Property;
-            if (s == TrackSettingsInterpreter::propertyName(Property::up))
-                return applyToUp;
-            if (s == TrackSettingsInterpreter::propertyName(Property::down))
-                return applyToDown;
-            if (s == TrackSettingsInterpreter::propertyName(Property::reversalsPerStepSize))
-                return applyToRunCount;
-            if (s == TrackSettingsInterpreter::propertyName(Property::stepSizes))
-                return applyToStepSize;
-            return nothing;
+static void applyToUp(TrackingSequence &sequence, int x) {
+    sequence.up = x;
+}
+
+static void applyToDown(TrackingSequence &sequence, int x) {
+    sequence.down = x;
+}
+
+static void applyToRunCount(TrackingSequence &sequence, int x) {
+    sequence.runCount = x;
+}
+
+static void applyToStepSize(TrackingSequence &sequence, int x) {
+    sequence.stepSize = x;
+}
+
+static void nothing(TrackingSequence &, int) {}
+
+static void(*propertyApplication(const std::string &s))(TrackingSequence &, int) {
+    using Property = TrackSettingsInterpreter::Property;
+    if (s == TrackSettingsInterpreter::propertyName(Property::up))
+        return applyToUp;
+    if (s == TrackSettingsInterpreter::propertyName(Property::down))
+        return applyToDown;
+    if (s == TrackSettingsInterpreter::propertyName(Property::reversalsPerStepSize))
+        return applyToRunCount;
+    if (s == TrackSettingsInterpreter::propertyName(Property::stepSizes))
+        return applyToStepSize;
+    return nothing;
+}
+
+const TrackingRule *TrackSettingsInterpreter::trackingRule(std::string s) {
+    rule_.clear();
+    auto stream = Stream{std::move(s)};
+    while (stream.nextLine()) {
+        auto sequenceCount{0U};
+        auto f = propertyApplication(stream.propertyName());
+        while (true) {
+            auto value = stream.value();
+            if (stream.failed())
+                break;
+            if (sequenceCount == rule_.size())
+                rule_.push_back({});
+            auto &sequence = rule_.at(sequenceCount++);
+            (*f)(sequence, value);
         }
     }
-    
-    const TrackingRule *TrackSettingsInterpreter::trackingRule(std::string s) {
-        rule_.clear();
-        auto stream = Stream{std::move(s)};
-        while (stream.nextLine()) {
-            auto sequenceCount{0U};
-            auto f = propertyApplication(stream.propertyName());
-            while (true) {
-                auto value = stream.value();
-                if (stream.failed())
-                    break;
-                if (sequenceCount == rule_.size())
-                    rule_.push_back({});
-                auto &sequence = rule_.at(sequenceCount++);
-                (*f)(sequence, value);
-            }
-        }
-        return &rule_;
-    }
+    return &rule_;
+}
 }
