d19bc04b5b12c3044ba91c026daf081af5fc5797
Seth Bashford <seth.bashford@boystown.org>
2020-01-15 16:01:11 -0600

    splitting

--- av-speech-in-noise/recognition-test/include/recognition-test/Model.hpp
+++ av-speech-in-noise/recognition-test/include/recognition-test/Model.hpp
@@ -2,6 +2,7 @@
 #define AV_SPEECH_IN_NOISE_RECOGNITION_TEST_INCLUDE_RECOGNITION_TEST_MODEL_HPP_
 
 #include "TargetList.hpp"
+#include "TestMethod.hpp"
 #include <av-speech-in-noise/Model.hpp>
 #include <vector>
 #include <string>
@@ -23,41 +24,6 @@ class Randomizer {
     virtual ~Randomizer() = default;
     virtual auto randomFloatBetween(double, double) -> double = 0;
     virtual auto randomIntBetween(int, int) -> int = 0;
-};
-
-class OutputFile {
-  public:
-    virtual ~OutputFile() = default;
-    virtual void openNewFile(const TestIdentity &) = 0;
-    class OpenFailure {};
-    virtual void writeTrial(
-        const coordinate_response_measure::AdaptiveTrial &) = 0;
-    virtual void writeTrial(
-        const coordinate_response_measure::FixedLevelTrial &) = 0;
-    virtual void writeTrial(const open_set::FreeResponseTrial &) = 0;
-    virtual void writeTrial(const open_set::AdaptiveTrial &) = 0;
-    virtual void writeTest(const AdaptiveTest &) = 0;
-    virtual void writeTest(const FixedLevelTest &) = 0;
-    virtual void close() = 0;
-    virtual void save() = 0;
-};
-
-class TestMethod {
-  public:
-    virtual ~TestMethod() = default;
-    virtual auto complete() -> bool = 0;
-    virtual auto next() -> std::string = 0;
-    virtual auto currentTarget() -> std::string = 0;
-    virtual auto snr_dB() -> int = 0;
-    virtual void submitCorrectResponse() = 0;
-    virtual void submitIncorrectResponse() = 0;
-    virtual void submitResponse(const open_set::FreeResponse &) = 0;
-    virtual void writeTestingParameters(OutputFile *) = 0;
-    virtual void writeLastCoordinateResponse(OutputFile *) = 0;
-    virtual void writeLastCorrectResponse(OutputFile *) = 0;
-    virtual void writeLastIncorrectResponse(OutputFile *) = 0;
-    virtual void submitResponse(
-        const coordinate_response_measure::Response &) = 0;
 };
 
 class TestConcluder {
