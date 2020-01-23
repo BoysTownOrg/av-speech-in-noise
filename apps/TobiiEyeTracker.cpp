417b163b1c12f824460ddafb963cf4f3297bbe26
Seth Bashford <seth.bashford@boystown.org>
2020-01-23 16:04:28 -0600

    crazyness

---
+++ apps/TobiiEyeTracker.cpp
@@ -0,0 +1,47 @@
+#include "TobiiEyeTracker.hpp"
+#include <gsl/gsl>
+#include <cmath>
+
+static auto eyeTracker(TobiiResearchEyeTrackers *eyeTrackers)
+    -> TobiiResearchEyeTracker * {
+    return eyeTrackers->count == 0U ? nullptr : eyeTrackers->eyetrackers[0];
+}
+
+TobiiEyeTracker::TobiiEyeTracker() {
+    if (tobii_research_find_all_eyetrackers(&eyeTrackers) !=
+        TOBII_RESEARCH_STATUS_OK)
+        throw int{};
+}
+
+TobiiEyeTracker::~TobiiEyeTracker() {
+    tobii_research_free_eyetrackers(eyeTrackers);
+}
+
+void TobiiEyeTracker::allocateRecordingTimeSeconds(double seconds) {
+    float gaze_output_frequency_Hz{};
+    tobii_research_get_gaze_output_frequency(
+        eyeTracker(eyeTrackers), &gaze_output_frequency_Hz);
+    gazeData.resize(gsl::narrow<std::size_t>(
+        std::ceil(gaze_output_frequency_Hz * seconds)));
+    head = 0;
+}
+
+void TobiiEyeTracker::start() {
+    tobii_research_subscribe_to_gaze_data(
+        eyeTracker(eyeTrackers), gaze_data_callback, this);
+}
+
+void TobiiEyeTracker::stop() {
+    tobii_research_unsubscribe_from_gaze_data(
+        eyeTracker(eyeTrackers), gaze_data_callback);
+}
+
+void TobiiEyeTracker::gaze_data_callback(
+    TobiiResearchGazeData *gaze_data, void *self) {
+    static_cast<TobiiEyeTracker *>(self)->gazeDataReceived(gaze_data);
+}
+
+void TobiiEyeTracker::gazeDataReceived(TobiiResearchGazeData *gaze_data) {
+    if (head < gazeData.size())
+        gazeData.at(head++) = *gaze_data;
+}
--- apps/TobiiEyeTracker.hpp
+++ apps/TobiiEyeTracker.hpp
@@ -1,47 +1,22 @@
-#ifndef APPS_TOBIIEYETRACKER_HPP_
-#define APPS_TOBIIEYETRACKER_HPP_
+#ifndef AV_SPEECH_IN_NOISE_APPS_TOBIIEYETRACKER_HPP_
+#define AV_SPEECH_IN_NOISE_APPS_TOBIIEYETRACKER_HPP_
 
-#include "tobii_research_streams.h"
 #include <recognition-test/RecognitionTestModel.hpp>
+#include <tobii_research_streams.h>
 #include <vector>
-
-static TobiiResearchEyeTracker *eyeTracker(
-    TobiiResearchEyeTrackers *eyeTrackers) {
-    return eyeTrackers->eyetrackers[0];
-}
 
 class TobiiEyeTracker : public av_speech_in_noise::EyeTracker {
   public:
-    TobiiEyeTracker() { tobii_research_find_all_eyetrackers(&eyeTrackers); }
-
-    ~TobiiEyeTracker() override {
-        tobii_research_free_eyetrackers(eyeTrackers);
-    }
-
-    void allocateRecordingTimeSeconds(double s) override {
-        gazeData.resize(60 * s);
-        head = 0;
-    }
-
-    void start() override {
-        tobii_research_subscribe_to_gaze_data(
-            eyeTracker(eyeTrackers), gaze_data_callback, this);
-    }
-
-    void stop() override {
-        tobii_research_unsubscribe_from_gaze_data(
-            eyeTracker(eyeTrackers), gaze_data_callback);
-    }
+    TobiiEyeTracker();
+    ~TobiiEyeTracker() override;
+    void allocateRecordingTimeSeconds(double s) override;
+    void start() override;
+    void stop() override;
 
   private:
     static void gaze_data_callback(
-        TobiiResearchGazeData *gaze_data, void *self) {
-        static_cast<TobiiEyeTracker *>(self)->gazeDataReceived(gaze_data);
-    }
-
-    void gazeDataReceived(TobiiResearchGazeData *gaze_data) {
-        gazeData.at(head++) = *gaze_data;
-    }
+        TobiiResearchGazeData *gaze_data, void *self);
+    void gazeDataReceived(TobiiResearchGazeData *gaze_data);
 
     std::vector<TobiiResearchGazeData> gazeData{};
     TobiiResearchEyeTrackers *eyeTrackers{};
