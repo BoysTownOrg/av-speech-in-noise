250c3505deba53a10eebb692b7ee210790a969f7
Seth Bashford <seth.bashford@boystown.org>
2020-01-27 15:45:28 -0600

    cool

--- apps/MersenneTwisterRandomizer.hpp
+++ apps/MersenneTwisterRandomizer.hpp
@@ -1,34 +1,33 @@
-#ifndef MersenneTwisterRandomizer_h
-#define MersenneTwisterRandomizer_h
+#ifndef APPS_MERSENNETWISTERRANDOMIZER_HPP_
+#define APPS_MERSENNETWISTERRANDOMIZER_HPP_
 
 #include <target-list/RandomizedTargetList.hpp>
 #include <recognition-test/RecognitionTestModel.hpp>
 #include <random>
 
-class MersenneTwisterRandomizer :
-    public target_list::Randomizer,
-    public av_speech_in_noise::Randomizer
-{
+class MersenneTwisterRandomizer : public target_list::Randomizer,
+                                  public av_speech_in_noise::Randomizer {
     std::mt19937 engine{std::random_device{}()};
-public:
+
+  public:
     void shuffle(shuffle_iterator begin, shuffle_iterator end) override {
         std::shuffle(begin, end, engine);
     }
 
-    void shuffle(int_shuffle_iterator begin, int_shuffle_iterator end) override {
+    void shuffle(
+        int_shuffle_iterator begin, int_shuffle_iterator end) override {
         std::shuffle(begin, end, engine);
     }
-    
-    double randomFloatBetween(double a, double b) override {
+
+    auto randomFloatBetween(double a, double b) -> double override {
         std::uniform_real_distribution<> distribution{a, b};
         return distribution(engine);
     }
-    
-    int randomIntBetween(int a, int b) override {
+
+    auto randomIntBetween(int a, int b) -> int override {
         std::uniform_int_distribution<> distribution{a, b};
         return distribution(engine);
     }
-    
 };
 
 #endif
