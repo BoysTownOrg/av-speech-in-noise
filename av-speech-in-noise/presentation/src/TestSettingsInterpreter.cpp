ff0924a3e734101a43ff2e2a1a5fc155051ca669
Seth Bashford <seth.bashford@boystown.org>
2020-03-02 10:50:09 -0600

    refactor

--- av-speech-in-noise/presentation/src/TestSettingsInterpreter.cpp
+++ av-speech-in-noise/presentation/src/TestSettingsInterpreter.cpp
@@ -1,9 +1,19 @@
 #include "TestSettingsInterpreter.hpp"
+#include <sstream>
 
 namespace av_speech_in_noise {
-void TestSettingsInterpreterImpl::apply(Model &model, const std::string &) {
+void TestSettingsInterpreterImpl::apply(
+    Model &model, const std::string &contents) {
     AdaptiveTest test;
-    test.targetListDirectory = "a";
+    std::stringstream stream{contents};
+    std::string line;
+    std::getline(stream, line);
+    std::getline(stream, line);
+    auto entryName{line.substr(0, line.find(':'))};
+    if (entryName == name(TestSetting::targets)) {
+        auto entry{line.substr(line.find(':') + 2)};
+        test.targetListDirectory = entry;
+    }
     model.initializeTest(test);
 }
 }
