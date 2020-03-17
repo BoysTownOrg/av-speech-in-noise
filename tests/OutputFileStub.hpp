452be6bee28f2caa3f6d255f59c6e494826d853d
Seth Bashford <seth.bashford@boystown.org>
2020-03-17 14:50:06 -0500

    refactor

--- tests/OutputFileStub.hpp
+++ tests/OutputFileStub.hpp
@@ -9,11 +9,11 @@
 namespace av_speech_in_noise {
 class OutputFileStub : public OutputFile {
     coordinate_response_measure::AdaptiveTrial
-        writtenAdaptiveCoordinateResponseTrial_{};
-    coordinate_response_measure::FixedLevelTrial writtenFixedLevelTrial_{};
-    open_set::FreeResponseTrial writtenFreeResponseTrial_{};
-    open_set::CorrectKeywordsTrial writtenCorrectKeywords_{};
-    open_set::AdaptiveTrial writtenOpenSetAdaptiveTrial_{};
+        adaptiveCoordinateResponseTrial_{};
+    coordinate_response_measure::FixedLevelTrial fixedLevelTrial_{};
+    open_set::FreeResponseTrial freeResponseTrial_{};
+    open_set::CorrectKeywordsTrial correctKeywords_{};
+    open_set::AdaptiveTrial openSetAdaptiveTrial_{};
     AdaptiveTestResult adaptiveTestResult_{};
     LogString log_{};
     const AdaptiveTest *adaptiveTest_{};
@@ -27,7 +27,7 @@ class OutputFileStub : public OutputFile {
     void write(
         const coordinate_response_measure::AdaptiveTrial &trial) override {
         addToLog("writeTrial ");
-        writtenAdaptiveCoordinateResponseTrial_ = trial;
+        adaptiveCoordinateResponseTrial_ = trial;
     }
 
     void openNewFile(const TestIdentity &p) override {
@@ -51,32 +51,30 @@ class OutputFileStub : public OutputFile {
 
     void write(const open_set::FreeResponseTrial &p) override {
         addToLog("writeTrial ");
-        writtenFreeResponseTrial_ = p;
+        freeResponseTrial_ = p;
     }
 
     void write(const open_set::CorrectKeywordsTrial &p) override {
         addToLog("writeTrial ");
-        writtenCorrectKeywords_ = p;
+        correctKeywords_ = p;
     }
 
     void write(const open_set::AdaptiveTrial &p) override {
         addToLog("writeTrial ");
-        writtenOpenSetAdaptiveTrial_ = p;
+        openSetAdaptiveTrial_ = p;
     }
 
     void write(
         const coordinate_response_measure::FixedLevelTrial &trial) override {
         addToLog("writeTrial ");
-        writtenFixedLevelTrial_ = trial;
+        fixedLevelTrial_ = trial;
     }
 
     void write(const AdaptiveTestResult &result) override {
         adaptiveTestResult_ = result;
     }
 
-    auto writtenFixedLevelTrial() const -> auto & {
-        return writtenFixedLevelTrial_;
-    }
+    auto writtenFixedLevelTrial() const -> auto & { return fixedLevelTrial_; }
 
     void addToLog(const std::string &s) { log_.insert(s); }
 
@@ -91,19 +89,17 @@ class OutputFileStub : public OutputFile {
     auto fixedLevelTest() const { return fixedLevelTest_; }
 
     auto writtenAdaptiveCoordinateResponseTrial() const -> auto & {
-        return writtenAdaptiveCoordinateResponseTrial_;
+        return adaptiveCoordinateResponseTrial_;
     }
 
     auto writtenFreeResponseTrial() const -> auto & {
-        return writtenFreeResponseTrial_;
+        return freeResponseTrial_;
     }
 
-    auto writtenCorrectKeywords() const -> auto & {
-        return writtenCorrectKeywords_;
-    }
+    auto writtenCorrectKeywords() const -> auto & { return correctKeywords_; }
 
     auto writtenOpenSetAdaptiveTrial() const -> auto & {
-        return writtenOpenSetAdaptiveTrial_;
+        return openSetAdaptiveTrial_;
     }
 
     auto adaptiveTestResult() const -> AdaptiveTestResult {
