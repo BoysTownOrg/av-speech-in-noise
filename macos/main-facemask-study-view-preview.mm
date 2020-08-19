61f425cc2289c8cd40d52be05ac53462df914c86
Seth Bashford <seth.bashford@boystown.org>
2020-08-19 16:31:00 -0500

    closer

--- macos/main-facemask-study-view-preview.mm
+++ macos/main-facemask-study-view-preview.mm
@@ -1,3 +1,10 @@
 #include "FacemaskStudySetupView.h"
 
-int main() {}
+int main() {
+    const auto viewController{[[NSTabViewController alloc] init]};
+    [viewController setTabStyle:NSTabViewControllerTabStyleUnspecified];
+    NSWindow *window{[NSWindow windowWithContentViewController:viewController]};
+    av_speech_in_noise::FacemaskStudySetupView view{viewController};
+    [window makeKeyAndOrderFront:nil];
+    [[NSApplication sharedApplication] run];
+}
