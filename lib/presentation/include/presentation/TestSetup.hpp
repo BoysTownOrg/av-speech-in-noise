5dc3c715c3e4d1eb4f1af059d5eb92a434e33411
Seth Bashford <seth.bashford@boystown.org>
2020-09-04 10:21:22 -0500

    s

--- lib/presentation/include/presentation/TestSetup.hpp
+++ lib/presentation/include/presentation/TestSetup.hpp
@@ -2,9 +2,41 @@
 #define AV_SPEECH_IN_NOISE_PRESENTATION_INCLUDE_PRESENTATION_TESTSETUP_HPP_
 
 #include "PresenterSimple.hpp"
+#include <vector>
 #include <string>
 
 namespace av_speech_in_noise {
+class TestSetupInputView {
+  public:
+    class EventListener {
+      public:
+        virtual ~EventListener() = default;
+        virtual void notifyThatConfirmButtonHasBeenClicked() = 0;
+        virtual void notifyThatPlayCalibrationButtonHasBeenClicked() = 0;
+        virtual void notifyThatBrowseForTestSettingsButtonHasBeenClicked() = 0;
+    };
+    virtual void subscribe(EventListener *) = 0;
+    virtual ~TestSetupInputView() = default;
+    virtual auto testSettingsFile() -> std::string = 0;
+    virtual auto startingSnr() -> std::string = 0;
+    virtual auto testerId() -> std::string = 0;
+    virtual auto subjectId() -> std::string = 0;
+    virtual auto session() -> std::string = 0;
+    virtual auto rmeSetting() -> std::string = 0;
+    virtual auto transducer() -> std::string = 0;
+};
+
+class TestSetupOutputView {
+  public:
+    virtual ~TestSetupOutputView() = default;
+    virtual void populateTransducerMenu(std::vector<std::string>) = 0;
+    virtual void setTestSettingsFile(std::string) = 0;
+    virtual void show() = 0;
+    virtual void hide() = 0;
+};
+
+class TestSetupView : public virtual TestSetupOutputView,
+                      public virtual TestSetupInputView {};
 enum class Method {
     adaptivePassFail,
     adaptivePassFailWithEyeTracking,
--- lib/presentation/include/presentation/View.hpp
+++ lib/presentation/include/presentation/View.hpp
@@ -5,38 +5,6 @@
 #include <string>
 
 namespace av_speech_in_noise {
-class TestSetupInputView {
-  public:
-    class EventListener {
-      public:
-        virtual ~EventListener() = default;
-        virtual void notifyThatConfirmButtonHasBeenClicked() = 0;
-        virtual void notifyThatPlayCalibrationButtonHasBeenClicked() = 0;
-        virtual void notifyThatBrowseForTestSettingsButtonHasBeenClicked() = 0;
-    };
-    virtual void subscribe(EventListener *) = 0;
-    virtual ~TestSetupInputView() = default;
-    virtual auto testSettingsFile() -> std::string = 0;
-    virtual auto startingSnr() -> std::string = 0;
-    virtual auto testerId() -> std::string = 0;
-    virtual auto subjectId() -> std::string = 0;
-    virtual auto session() -> std::string = 0;
-    virtual auto rmeSetting() -> std::string = 0;
-    virtual auto transducer() -> std::string = 0;
-};
-
-class TestSetupOutputView {
-  public:
-    virtual ~TestSetupOutputView() = default;
-    virtual void populateTransducerMenu(std::vector<std::string>) = 0;
-    virtual void setTestSettingsFile(std::string) = 0;
-    virtual void show() = 0;
-    virtual void hide() = 0;
-};
-
-class TestSetupView : public virtual TestSetupOutputView,
-                      public virtual TestSetupInputView {};
-
 class View {
   public:
     virtual ~View() = default;
