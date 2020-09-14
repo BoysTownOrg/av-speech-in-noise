fd4700fa0d048c74fa5d564b67bf5dc58a83f22a
Seth Bashford <seth.bashford@boystown.org>
2020-09-14 11:44:51 -0500

    s

--- lib/presentation/include/presentation/View.hpp
+++ lib/presentation/include/presentation/View.hpp
@@ -1,10 +1,12 @@
 #ifndef AV_SPEECH_IN_NOISE_PRESENTATION_INCLUDE_PRESENTATION_VIEW_HPP_
 #define AV_SPEECH_IN_NOISE_PRESENTATION_INCLUDE_PRESENTATION_VIEW_HPP_
 
+#include "Interface.hpp"
+
 namespace av_speech_in_noise {
 class View {
   public:
-    virtual ~View() = default;
+    AV_SPEECH_IN_NOISE_INTERFACE_SPECIAL_MEMBER_FUNCTIONS(View);
     virtual void show() = 0;
     virtual void hide() = 0;
 };
