3a2633ac55dff5106fbccfe5e675b7ce77b2a390
Seth Bashford <seth.bashford@boystown.org>
2020-09-15 11:02:05 -0500

    s

--- test/Test.cpp
+++ test/Test.cpp
@@ -1,5 +1,6 @@
 #include "assert-utility.hpp"
 #include "ModelStub.hpp"
+#include "TestViewStub.hpp"
 #include "SessionViewStub.hpp"
 #include <presentation/TestImpl.hpp>
 #include <av-speech-in-noise/name.hpp>
@@ -9,103 +10,6 @@
 
 namespace av_speech_in_noise {
 namespace {
-class TestUIStub : public TestView, public TestControl {
-  public:
-    void declineContinuingTesting() { listener_->declineContinuingTesting(); }
-
-    [[nodiscard]] auto continueTestingDialogMessage() const -> std::string {
-        return continueTestingDialogMessage_;
-    }
-
-    void setContinueTestingDialogMessage(const std::string &s) override {
-        continueTestingDialogMessage_ = s;
-    }
-
-    void acceptContinuingTesting() { listener_->acceptContinuingTesting(); }
-
-    void showContinueTestingDialog() override {
-        continueTestingDialogShown_ = true;
-    }
-
-    [[nodiscard]] auto continueTestingDialogShown() const -> bool {
-        return continueTestingDialogShown_;
-    }
-
-    void hideContinueTestingDialog() override {
-        continueTestingDialogHidden_ = true;
-    }
-
-    [[nodiscard]] auto continueTestingDialogHidden() const -> bool {
-        return continueTestingDialogHidden_;
-    }
-
-    void show() override { shown_ = true; }
-
-    [[nodiscard]] auto shown() const { return shown_; }
-
-    void hide() override { hidden_ = true; }
-
-    [[nodiscard]] auto hidden() const { return hidden_; }
-
-    void attach(TestControl::Observer *e) override { listener_ = e; }
-
-    void exitTest() { listener_->exitTest(); }
-
-    void notifyThatPlayTrialButtonHasBeenClicked() { listener_->playTrial(); }
-
-    void display(std::string s) override { displayed_ = std::move(s); }
-
-    [[nodiscard]] auto displayed() const { return displayed_; }
-
-    void secondaryDisplay(std::string s) override {
-        secondaryDisplayed_ = std::move(s);
-    }
-
-    [[nodiscard]] auto secondaryDisplayed() const {
-        return secondaryDisplayed_;
-    }
-
-    void showNextTrialButton() override { nextTrialButtonShown_ = true; }
-
-    [[nodiscard]] auto nextTrialButtonShown() const {
-        return nextTrialButtonShown_;
-    }
-
-    void hideNextTrialButton() override { nextTrialButtonHidden_ = true; }
-
-    [[nodiscard]] auto nextTrialButtonHidden() const {
-        return nextTrialButtonHidden_;
-    }
-
-    void showExitTestButton() override { exitTestButtonShown_ = true; }
-
-    [[nodiscard]] auto exitTestButtonShown() const {
-        return exitTestButtonShown_;
-    }
-
-    void hideExitTestButton() override { exitTestButtonHidden_ = true; }
-
-    [[nodiscard]] auto exitTestButtonHidden() const {
-        return exitTestButtonHidden_;
-    }
-
-  private:
-    std::string displayed_;
-    std::string secondaryDisplayed_;
-    std::string continueTestingDialogMessage_;
-    std::string response_;
-    std::string correctKeywords_{"0"};
-    TestControl::Observer *listener_{};
-    bool exitTestButtonHidden_{};
-    bool exitTestButtonShown_{};
-    bool nextTrialButtonShown_{};
-    bool nextTrialButtonHidden_{};
-    bool continueTestingDialogShown_{};
-    bool continueTestingDialogHidden_{};
-    bool shown_{};
-    bool hidden_{};
-};
-
 class TestControlStub : public TestControl {
   public:
     void declineContinuingTesting() { listener_->declineContinuingTesting(); }
@@ -487,7 +391,7 @@ class ExperimenterTests : public ::testing::Test {
   protected:
     ModelStub model;
     SessionViewStub view;
-    TestUIStub experimenterView;
+    TestViewStub experimenterView;
     TestControlStub control;
     TaskControllerStub consonantController;
     TaskControllerStub coordinateResponseMeasureController;
--- test/TestViewStub.hpp
+++ test/TestViewStub.hpp
@@ -6,6 +6,58 @@
 namespace av_speech_in_noise {
 class TestViewStub : public TestView {
   public:
+    void declineContinuingTesting() { listener_->declineContinuingTesting(); }
+
+    [[nodiscard]] auto continueTestingDialogMessage() const -> std::string {
+        return continueTestingDialogMessage_;
+    }
+
+    void setContinueTestingDialogMessage(const std::string &s) override {
+        continueTestingDialogMessage_ = s;
+    }
+
+    void acceptContinuingTesting() { listener_->acceptContinuingTesting(); }
+
+    void showContinueTestingDialog() override {
+        continueTestingDialogShown_ = true;
+    }
+
+    [[nodiscard]] auto continueTestingDialogShown() const -> bool {
+        return continueTestingDialogShown_;
+    }
+
+    void hideContinueTestingDialog() override {
+        continueTestingDialogHidden_ = true;
+    }
+
+    [[nodiscard]] auto continueTestingDialogHidden() const -> bool {
+        return continueTestingDialogHidden_;
+    }
+
+    void show() override { shown_ = true; }
+
+    [[nodiscard]] auto shown() const { return shown_; }
+
+    void hide() override { hidden_ = true; }
+
+    [[nodiscard]] auto hidden() const { return hidden_; }
+
+    void exitTest() { listener_->exitTest(); }
+
+    void notifyThatPlayTrialButtonHasBeenClicked() { listener_->playTrial(); }
+
+    void display(std::string s) override { displayed_ = std::move(s); }
+
+    [[nodiscard]] auto displayed() const { return displayed_; }
+
+    void secondaryDisplay(std::string s) override {
+        secondaryDisplayed_ = std::move(s);
+    }
+
+    [[nodiscard]] auto secondaryDisplayed() const {
+        return secondaryDisplayed_;
+    }
+
     void showNextTrialButton() override { nextTrialButtonShown_ = true; }
 
     [[nodiscard]] auto nextTrialButtonShown() const {
@@ -18,19 +70,33 @@ class TestViewStub : public TestView {
         return nextTrialButtonHidden_;
     }
 
-    void setContinueTestingDialogMessage(const std::string &) override {}
-    void showContinueTestingDialog() override {}
-    void hideContinueTestingDialog() override {}
-    void show() override {}
-    void hide() override {}
-    void display(std::string) override {}
-    void secondaryDisplay(std::string) override {}
-    void showExitTestButton() override {}
-    void hideExitTestButton() override {}
+    void showExitTestButton() override { exitTestButtonShown_ = true; }
+
+    [[nodiscard]] auto exitTestButtonShown() const {
+        return exitTestButtonShown_;
+    }
+
+    void hideExitTestButton() override { exitTestButtonHidden_ = true; }
+
+    [[nodiscard]] auto exitTestButtonHidden() const {
+        return exitTestButtonHidden_;
+    }
 
   private:
+    std::string displayed_;
+    std::string secondaryDisplayed_;
+    std::string continueTestingDialogMessage_;
+    std::string response_;
+    std::string correctKeywords_{"0"};
+    TestControl::Observer *listener_{};
+    bool exitTestButtonHidden_{};
+    bool exitTestButtonShown_{};
     bool nextTrialButtonShown_{};
     bool nextTrialButtonHidden_{};
+    bool continueTestingDialogShown_{};
+    bool continueTestingDialogHidden_{};
+    bool shown_{};
+    bool hidden_{};
 };
 }
 
