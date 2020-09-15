15e49c9a9431927c65b422d66526a184d22b0cfc
Seth Bashford <seth.bashford@boystown.org>
2020-09-15 15:12:29 -0500

    s

--- test/SessionController.cpp
+++ test/SessionController.cpp
@@ -1,55 +1,12 @@
 #include "assert-utility.hpp"
 #include "ModelStub.hpp"
+#include "SessionViewStub.hpp"
 #include <presentation/SessionControllerImpl.hpp>
 #include <gtest/gtest.h>
 #include <utility>
 
 namespace av_speech_in_noise {
 namespace {
-class ViewStub : public SessionView {
-  public:
-    void setAudioDevice(std::string s) { audioDevice_ = std::move(s); }
-
-    auto audioDevice() -> std::string override { return audioDevice_; }
-
-    void showErrorMessage(std::string s) override {
-        errorMessage_ = std::move(s);
-    }
-
-    auto errorMessage() { return errorMessage_; }
-
-    void eventLoop() override { eventLoopCalled_ = true; }
-
-    auto browseForDirectory() -> std::string override {
-        return browseForDirectoryResult_;
-    }
-
-    auto browseCancelled() -> bool override { return browseCancelled_; }
-
-    auto browseForOpeningFile() -> std::string override {
-        return browseForOpeningFileResult_;
-    }
-
-    void setBrowseForOpeningFileResult(std::string s) {
-        browseForOpeningFileResult_ = std::move(s);
-    }
-
-    void setBrowseCancelled() { browseCancelled_ = true; }
-
-    void populateAudioDeviceMenu(std::vector<std::string> v) override {
-        audioDevices_ = std::move(v);
-    }
-
-  private:
-    std::vector<std::string> audioDevices_;
-    std::string errorMessage_;
-    std::string browseForDirectoryResult_;
-    std::string browseForOpeningFileResult_;
-    std::string audioDevice_;
-    bool eventLoopCalled_{};
-    bool browseCancelled_{};
-};
-
 class TestPresenterStub : public TestPresenter {
   public:
     void initialize(Method m) { method_ = m; }
@@ -87,7 +44,7 @@ class TestSetupPresenterStub : public TestSetupPresenter {
 class SessionControllerTests : public ::testing::Test {
   protected:
     ModelStub model;
-    ViewStub view;
+    SessionViewStub view;
     TestSetupPresenterStub testSetupPresenter;
     TestPresenterStub testPresenter;
     SessionControllerImpl controller{
