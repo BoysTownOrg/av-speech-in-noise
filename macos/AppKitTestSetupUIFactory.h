71d2ff87241ccd29d9464764c9d28f1d5bf23d46
Seth Bashford <seth.bashford@boystown.org>
2020-11-24 10:08:27 -0600

    same

--- macos/AppKitTestSetupUIFactory.h
+++ macos/AppKitTestSetupUIFactory.h
@@ -1,8 +1,9 @@
-#ifndef AV_SPEECH_IN_NOISE_MACOS_MACOSTESTSETUPVIEWFACTORY_H_
-#define AV_SPEECH_IN_NOISE_MACOS_MACOSTESTSETUPVIEWFACTORY_H_
+#ifndef AV_SPEECH_IN_NOISE_MACOS_APPKITTESTSETUPVIEWFACTORY_H_
+#define AV_SPEECH_IN_NOISE_MACOS_APPKITTESTSETUPVIEWFACTORY_H_
 
 #include <presentation/TestSetup.hpp>
-#import <Cocoa/Cocoa.h>
+#include <av-speech-in-noise/Interface.hpp>
+#import <AppKit/AppKit.h>
 #include <memory>
 
 namespace av_speech_in_noise {
@@ -11,7 +12,8 @@ class TestSetupUI : public virtual TestSetupView,
 
 class AppKitTestSetupUIFactory {
   public:
-    virtual ~AppKitTestSetupUIFactory() = default;
+    AV_SPEECH_IN_NOISE_INTERFACE_SPECIAL_MEMBER_FUNCTIONS(
+        AppKitTestSetupUIFactory);
     virtual auto make(NSViewController *) -> std::unique_ptr<TestSetupUI> = 0;
 };
 }
