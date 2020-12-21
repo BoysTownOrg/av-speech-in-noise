1b77a209ec2d80d1c98b2c12a1de397ee0adfb20
Seth Bashford <seth.bashford@boystown.org>
2020-12-21 15:03:48 -0600

    refactor

--- test/AudioReaderSimplified.cpp
+++ test/AudioReaderSimplified.cpp
@@ -1,42 +1,5 @@
-#include <av-speech-in-noise/Interface.hpp>
-#include <av-speech-in-noise/Model.hpp>
-#include <stimulus-players/AudioReader.hpp>
-#include <gsl/gsl>
-#include <string>
-#include <vector>
-#include <algorithm>
-
-namespace av_speech_in_noise {
-class BufferedAudioReaderSimple {
-  public:
-    AV_SPEECH_IN_NOISE_INTERFACE_SPECIAL_MEMBER_FUNCTIONS(
-        BufferedAudioReaderSimple);
-    virtual void load(const LocalUrl &) = 0;
-    virtual auto failed() -> bool = 0;
-    virtual auto channel(gsl::index) -> std::vector<float> = 0;
-    virtual auto channels() -> gsl::index = 0;
-};
-
-class AudioReaderSimplified : public AudioReader {
-  public:
-    AudioReaderSimplified(BufferedAudioReaderSimple &reader) : reader{reader} {}
-
-    auto read(std::string filePath) -> audio_type override {
-        reader.load(LocalUrl{filePath});
-        if (reader.failed())
-            throw InvalidFile{};
-        audio_type audio(reader.channels());
-        std::generate(audio.begin(), audio.end(),
-            [&, n = 0]() mutable { return reader.channel(n++); });
-        return audio;
-    }
-
-  private:
-    BufferedAudioReaderSimple &reader;
-};
-}
-
 #include "assert-utility.hpp"
+#include <stimulus-players/AudioReaderSimplified.hpp>
 #include <gtest/gtest.h>
 #include <gsl/gsl>
 #include <vector>
