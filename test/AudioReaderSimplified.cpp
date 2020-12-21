cdb0976bb5963d9ed3550faa8881df3f5badb095
Seth Bashford <seth.bashford@boystown.org>
2020-12-21 14:37:44 -0600

    refactor

--- test/AudioReaderSimplified.cpp
+++ test/AudioReaderSimplified.cpp
@@ -15,7 +15,8 @@ class BufferedAudioReaderSimple {
 class AudioReaderSimplified : public AudioReader {
   public:
     AudioReaderSimplified(BufferedAudioReaderSimple &reader) : reader{reader} {}
-    auto read(std::string filePath) -> audio_type {
+
+    auto read(std::string filePath) -> audio_type override {
         reader.load(LocalUrl{filePath});
         if (reader.failed())
             throw InvalidFile{};
@@ -39,19 +40,23 @@ class BufferedAudioReaderSimpleStub : public BufferedAudioReaderSimple {
 
     [[nodiscard]] auto url() const -> LocalUrl { return url_; }
 
-    void load(const LocalUrl &s) {
+    void load(const LocalUrl &s) override {
         url_ = s;
         if (failOnLoad_)
             failed_ = true;
     }
 
-    auto failed() -> bool { return failed_; }
+    auto failed() -> bool override { return failed_; }
 
   private:
     LocalUrl url_{};
     bool failOnLoad_{};
     bool failed_{};
 };
+
+auto read(AudioReaderSimplified &reader, std::string s) -> audio_type {
+    return reader.read(std::move(s));
+}
 
 class AudioReaderSimplifiedTests : public ::testing::Test {
   protected:
@@ -60,7 +65,7 @@ class AudioReaderSimplifiedTests : public ::testing::Test {
 };
 
 TEST_F(AudioReaderSimplifiedTests, loadsFile) {
-    reader.read("a");
+    read(reader, "a");
     AV_SPEECH_IN_NOISE_EXPECT_EQUAL(
         std::string{"a"}, bufferedReader.url().path);
 }
@@ -68,7 +73,7 @@ TEST_F(AudioReaderSimplifiedTests, loadsFile) {
 TEST_F(AudioReaderSimplifiedTests, readThrowsInvalidFileOnFailure) {
     bufferedReader.failOnLoad();
     try {
-        reader.read("a");
+        read(reader, "a");
         FAIL() << "Expected AudioReaderSimplified::InvalidFile";
     } catch (const AudioReaderSimplified::InvalidFile &) {
     }
