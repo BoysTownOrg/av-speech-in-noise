44c501b74b20c9e05ad951177573e3055342abef
Seth Bashford <seth.bashford@boystown.org>
2021-01-26 11:06:56 -0600

    add test

---
+++ lib/presentation/include/presentation/Syllables.hpp
@@ -0,0 +1,46 @@
+#ifndef AV_SPEECH_IN_NOISE_PRESENTATION_INCLUDE_PRESENTATION_SYLLABLES_HPP_
+#define AV_SPEECH_IN_NOISE_PRESENTATION_INCLUDE_PRESENTATION_SYLLABLES_HPP_
+
+#include "Task.hpp"
+#include <av-speech-in-noise/Interface.hpp>
+
+namespace av_speech_in_noise {
+class SyllablesView {
+  public:
+    AV_SPEECH_IN_NOISE_INTERFACE_SPECIAL_MEMBER_FUNCTIONS(SyllablesView);
+    virtual void hide() = 0;
+};
+
+class SyllablesControl {
+  public:
+    class Observer {
+      public:
+        AV_SPEECH_IN_NOISE_INTERFACE_SPECIAL_MEMBER_FUNCTIONS(Observer);
+    };
+    AV_SPEECH_IN_NOISE_INTERFACE_SPECIAL_MEMBER_FUNCTIONS(SyllablesControl);
+};
+
+class SyllablesPresenter {
+  public:
+    AV_SPEECH_IN_NOISE_INTERFACE_SPECIAL_MEMBER_FUNCTIONS(SyllablesPresenter);
+};
+
+class SyllablesController : public SyllablesControl::Observer {
+  public:
+    explicit SyllablesController(SyllablesControl &) {}
+};
+
+class SyllablesPresenterImpl : public SyllablesPresenter, public TaskPresenter {
+  public:
+    explicit SyllablesPresenterImpl(SyllablesView &view) : view{view} {}
+    void start() override {}
+    void stop() override {}
+    void showResponseSubmission() override {}
+    void hideResponseSubmission() override { view.hide(); }
+
+  private:
+    SyllablesView &view;
+};
+}
+
+#endif
---
+++ test/Syllables.cpp
@@ -0,0 +1,73 @@
+#include "assert-utility.hpp"
+#include <presentation/Syllables.hpp>
+#include <gtest/gtest.h>
+
+namespace av_speech_in_noise {
+namespace {
+class SyllablesViewStub : public SyllablesView {
+  public:
+    [[nodiscard]] auto hidden() const -> bool { return hidden_; }
+
+    void hide() { hidden_ = true; }
+
+  private:
+    bool hidden_{};
+};
+
+class SyllablesControlStub : public SyllablesControl {};
+
+class SyllablesPresenterStub : public SyllablesPresenter {};
+
+class SyllablesControllerTests : public ::testing::Test {
+  protected:
+    SyllablesControlStub control;
+    SyllablesController controller{control};
+};
+
+class SyllablesPresenterTests : public ::testing::Test {
+  protected:
+    SyllablesViewStub view;
+    SyllablesPresenterImpl presenter{view};
+};
+
+#define SYLLABLES_CONTROLLER_TEST(a) TEST_F(SyllablesControllerTests, a)
+
+#define SYLLABLES_PRESENTER_TEST(a) TEST_F(SyllablesPresenterTests, a)
+
+SYLLABLES_PRESENTER_TEST(hidesResponseSubmission) {
+    presenter.hideResponseSubmission();
+    AV_SPEECH_IN_NOISE_EXPECT_TRUE(view.hidden());
+}
+
+// SYLLABLES_PRESENTER_TEST(hidesResponseButtonsWhenStopped) {
+//     presenter.stop();
+//     AV_SPEECH_IN_NOISE_EXPECT_RESPONSE_BUTTONS_HIDDEN(view);
+// }
+
+// SYLLABLES_PRESENTER_TEST(showsReadyButtonWhenStarted) {
+//     presenter.start();
+//     AV_SPEECH_IN_NOISE_EXPECT_TRUE(testView.nextTrialButtonShown());
+// }
+
+// SYLLABLES_PRESENTER_TEST(showsResponseButtonWhenShowingResponseSubmission) {
+//     presenter.showResponseSubmission();
+//     AV_SPEECH_IN_NOISE_EXPECT_TRUE(view.responseSubmissionShown());
+// }
+
+// SYLLABLES_CONTROLLER_TEST(submitsKeywordResponseAfterSubmitButtonIsClicked) {
+//     control.setFirstKeywordCorrect();
+//     control.setThirdKeywordCorrect();
+//     notifyThatSubmitButtonHasBeenClicked(control);
+//     AV_SPEECH_IN_NOISE_EXPECT_TRUE(model.threeKeywords().firstCorrect);
+//     AV_SPEECH_IN_NOISE_EXPECT_FALSE(model.threeKeywords().secondCorrect);
+//     AV_SPEECH_IN_NOISE_EXPECT_TRUE(model.threeKeywords().thirdCorrect);
+// }
+
+// SYLLABLES_CONTROLLER_TEST(
+//     notifiesThatUserIsReadyForNextTrialAfterSubmitButtonIsClicked) {
+//     notifyThatSubmitButtonHasBeenClicked(control);
+//     AV_SPEECH_IN_NOISE_EXPECT_TRUE(
+//         testController.notifiedThatUserIsDoneResponding());
+// }
+}
+}
