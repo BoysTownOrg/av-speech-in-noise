66c966c8b5e6d0f941396e4861b1d7755baf98be
Seth Bashford <seth.bashford@boystown.org>
2021-04-29 13:28:43 -0500

    refactor

--- lib/recognition-test/include/recognition-test/EyeTrackerCalibration.hpp
+++ lib/recognition-test/include/recognition-test/EyeTrackerCalibration.hpp
@@ -30,32 +30,11 @@ class EyeTrackerCalibrator {
     virtual void calibrate(Point) = 0;
 };
 
-static auto consumeOne(std::vector<Point> &x) -> Point {
-    auto point = x.front();
-    x.erase(x.begin());
-    return point;
-}
-
-static void present(IPresenter &presenter, const std::vector<Point> &points) {
-    if (!points.empty())
-        presenter.present(points.front());
-}
-
 class Interactor : public IPresenter::Observer {
   public:
-    explicit Interactor(IPresenter &presenter, EyeTrackerCalibrator &calibrator,
-        std::vector<Point> points)
-        : points{std::move(points)}, presenter{presenter}, calibrator{
-                                                               calibrator} {
-        presenter.attach(this);
-    }
-
-    void notifyThatPointIsReady() override {
-        calibrator.calibrate(consumeOne(points));
-        present(presenter, points);
-    }
-
-    void calibrate() { present(presenter, points); }
+    Interactor(IPresenter &, EyeTrackerCalibrator &, std::vector<Point>);
+    void notifyThatPointIsReady() override;
+    void calibrate();
 
   private:
     std::vector<Point> points;
---
+++ lib/recognition-test/src/EyeTrackerCalibration.cpp
@@ -0,0 +1,27 @@
+#include "EyeTrackerCalibration.hpp"
+
+namespace av_speech_in_noise::eye_tracker_calibration {
+static auto consumeOne(std::vector<Point> &x) -> Point {
+    auto point = x.front();
+    x.erase(x.begin());
+    return point;
+}
+
+static void present(IPresenter &presenter, const std::vector<Point> &points) {
+    if (!points.empty())
+        presenter.present(points.front());
+}
+
+Interactor::Interactor(IPresenter &presenter, EyeTrackerCalibrator &calibrator,
+    std::vector<Point> points)
+    : points{std::move(points)}, presenter{presenter}, calibrator{calibrator} {
+    presenter.attach(this);
+}
+
+void Interactor::notifyThatPointIsReady() {
+    calibrator.calibrate(consumeOne(points));
+    present(presenter, points);
+}
+
+void Interactor::calibrate() { present(presenter, points); }
+}
