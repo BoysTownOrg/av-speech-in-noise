ffd58c7de33496493f7240dd3f10f6e33674521a
Seth Bashford <seth.bashford@boystown.org>
2021-06-01 15:18:32 -0500

    refactor

--- lib/core/include/av-speech-in-noise/core/SubmittingKeywords.hpp
+++ lib/core/include/av-speech-in-noise/core/SubmittingKeywords.hpp
@@ -5,29 +5,12 @@
 #include "IOutputFile.hpp"
 #include "IRecognitionTestModel.hpp"
 #include "IFixedLevelMethod.hpp"
-#include <filesystem>
 
 namespace av_speech_in_noise::submitting_keywords {
 class InteractorImpl : public Interactor {
   public:
-    InteractorImpl(FixedLevelMethod &method,
-        RecognitionTestModel &recognitionTestModel, OutputFile &outputFile)
-        : method{method}, recognitionTestModel{recognitionTestModel},
-          outputFile{outputFile} {}
-
-    void submit(const ThreeKeywordsResponse &p) override {
-        method.submit(p);
-        ThreeKeywordsTrial trial;
-        trial.firstCorrect = p.firstCorrect;
-        trial.secondCorrect = p.secondCorrect;
-        trial.thirdCorrect = p.thirdCorrect;
-        trial.target =
-            std::filesystem::path{method.currentTarget().path}.filename();
-        trial.flagged = p.flagged;
-        outputFile.write(trial);
-        outputFile.save();
-        recognitionTestModel.prepareNextTrialIfNeeded();
-    }
+    InteractorImpl(FixedLevelMethod &, RecognitionTestModel &, OutputFile &);
+    void submit(const ThreeKeywordsResponse &p) override;
 
   private:
     FixedLevelMethod &method;
