401702b93a7a81d5498df8262188d994fa96061a
Seth Bashford <seth.bashford@boystown.org>
2021-06-01 14:03:29 -0500

    refactor

--- lib/core/include/av-speech-in-noise/core/SubmittingPassFail.hpp
+++ lib/core/include/av-speech-in-noise/core/SubmittingPassFail.hpp
@@ -9,24 +9,9 @@
 namespace av_speech_in_noise::submitting_pass_fail {
 class InteractorImpl : public Interactor {
   public:
-    InteractorImpl(AdaptiveMethod &adaptiveMethod, RecognitionTestModel &model,
-        OutputFile &outputFile)
-        : adaptiveMethod{adaptiveMethod}, model{model}, outputFile{outputFile} {
-    }
-
-    void submitCorrectResponse() override {
-        adaptiveMethod.submitCorrectResponse();
-        adaptiveMethod.writeLastCorrectResponse(outputFile);
-        outputFile.save();
-        model.prepareNextTrialIfNeeded();
-    }
-
-    void submitIncorrectResponse() override {
-        adaptiveMethod.submitIncorrectResponse();
-        adaptiveMethod.writeLastIncorrectResponse(outputFile);
-        outputFile.save();
-        model.prepareNextTrialIfNeeded();
-    }
+    InteractorImpl(AdaptiveMethod &, RecognitionTestModel &, OutputFile &);
+    void submitCorrectResponse() override;
+    void submitIncorrectResponse() override;
 
   private:
     AdaptiveMethod &adaptiveMethod;
