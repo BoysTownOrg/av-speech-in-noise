823a7d2e4bdbb6db20d6fbc66f1822e11ab36250
Seth Bashford <seth.bashford@boystown.org>
2021-11-30 15:35:33 -0600

    same idea as before (I saw this in the reference docs)

--- macos/SwiftUI.swift
+++ macos/SwiftUI.swift
@@ -103,19 +103,24 @@ struct SettingsView: View {
     }
 
     var body: some View {
-        Form {
-            Picker("Audio Device:", selection: $audioDevice.string) {
-                ForEach(audioDevices.items) {
-                    Text($0.string)
+        Spacer()
+        HStack {
+            Spacer()
+            Form {
+                Picker("Audio Device:", selection: $audioDevice.string) {
+                    ForEach(audioDevices.items) {
+                        Text($0.string)
+                    }
+                }
+                Picker("Subject Screen:", selection: $subjectScreen.string) {
+                    ForEach(subjectScreens.items) {
+                        Text($0.string)
+                    }
                 }
             }
-            Picker("Subject Screen:", selection: $subjectScreen.string) {
-                ForEach(subjectScreens.items) {
-                    Text($0.string)
-                }
-            }
+            Spacer()
         }
-        .padding()
+        Spacer()
     }
 }
 
