527c6d764f5540256e173c11fadd5cb00edec98e
Seth Bashford <seth.bashford@boystown.org>
2022-01-20 10:53:16 -0600

    Revert "form is not doing what I thought it did, switching to vstack. Otherwise window is snapped to max vertical screen height"
    This reverts commit a6981781805a80f64e6c47ec3b5c2a7f59786d06.

--- macos/SwiftUI.swift
+++ macos/SwiftUI.swift
@@ -226,7 +226,7 @@ struct SwiftTestSetupView: View {
             Spacer()
             HStack {
                 Spacer()
-                VStack {
+                Form {
                     TextField(
                         "Subject ID:",
                         text: $subjectId.string
