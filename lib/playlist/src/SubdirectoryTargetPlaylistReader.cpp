3c988366454394fea7ba9f151b1b4221f70d913b
Seth Bashford <seth.bashford@boystown.org>
2022-03-01 10:23:09 -0600

    refactor

--- lib/playlist/src/SubdirectoryTargetPlaylistReader.cpp
+++ lib/playlist/src/SubdirectoryTargetPlaylistReader.cpp
@@ -1,4 +1,5 @@
 #include "SubdirectoryTargetPlaylistReader.hpp"
+#include "av-speech-in-noise/Model.hpp"
 
 #include <filesystem>
 
@@ -7,20 +8,25 @@ SubdirectoryTargetPlaylistReader::SubdirectoryTargetPlaylistReader(
     TargetPlaylistFactory *targetListFactory, DirectoryReader *directoryReader)
     : targetListFactory{targetListFactory}, directoryReader{directoryReader} {}
 
+static void add(TargetPlaylistReader::lists_type &lists,
+    TargetPlaylistFactory *factory, const LocalUrl &url) {
+    auto list{factory->make()};
+    list->loadFromDirectory(url);
+    lists.push_back(std::move(list));
+}
+
 auto SubdirectoryTargetPlaylistReader::read(const LocalUrl &directory)
     -> lists_type {
     lists_type lists{};
     try {
         const auto subDirectories = directoryReader->subDirectories(directory);
-        for (const auto &subDirectory : subDirectories) {
-            lists.push_back(targetListFactory->make());
-            std::filesystem::path fullPath{directory.path};
-            lists.back()->loadFromDirectory({fullPath / subDirectory.path});
-        }
-        if (subDirectories.empty()) {
-            lists.push_back(targetListFactory->make());
-            lists.back()->loadFromDirectory(directory);
-        }
+        for (const auto &subDirectory : subDirectories)
+            add(lists, targetListFactory,
+                LocalUrl{
+                    std::filesystem::path{directory.path} / subDirectory.path});
+        if (subDirectories.empty())
+            add(lists, targetListFactory, directory);
+
     } catch (const DirectoryReader::CannotRead &e) {
     }
     return lists;
