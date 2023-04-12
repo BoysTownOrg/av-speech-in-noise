0c3974d797c65e577619d846536f99fbd720cdab
Seth Bashford <seth.bashford@boystown.org>
2023-04-12 14:56:53 -0500

    refactor to objective-c++

--- macos/masking-images.mm
+++ macos/masking-images.mm
@@ -5,11 +5,12 @@
 #include <Foundation/Foundation.h>
 #include <CoreGraphics/CoreGraphics.h>
 
-@interface View : NSView
+@interface ImageView : NSView
 @property CGImageRef image;
+- (instancetype)initWithImage:(CGImageRef)image;
 @end
 
-@implementation View
+@implementation ImageView
 - (id)initWithImage:(CGImageRef)image {
     self = [super initWithFrame:NSMakeRect(0, 0, CGImageGetWidth(image),
                                     CGImageGetHeight(image))];
@@ -20,7 +21,6 @@
 - (void)drawRect:(NSRect)rect {
     CGContextRef myContext = [[NSGraphicsContext // 1
         currentContext] CGContext];
-    // ********** Your drawing code here ********** // 2
     CGContextDrawImage(myContext,
         CGRectMake(
             0, 0, CGImageGetWidth(self.image), CGImageGetHeight(self.image)),
@@ -32,33 +32,15 @@
 
 @end
 
-CGContextRef MyCreateBitmapContext(int pixelsWide, int pixelsHigh)
-
-{
-    CGContextRef context = NULL;
-    CGColorSpaceRef colorSpace;
-    void *bitmapData;
-    int bitmapByteCount;
-    int bitmapBytesPerRow;
-    bitmapBytesPerRow = (pixelsWide * 3); // 1
-    bitmapByteCount = (bitmapBytesPerRow * pixelsHigh);
-    colorSpace = CGColorSpaceCreateDeviceGray(); // 2
-    bitmapData = calloc(bitmapByteCount, sizeof(uint8_t)); // 3
-    if (bitmapData == NULL) {
-        fprintf(stderr, "Memory not allocated!");
-        return NULL;
-    }
-    context = CGBitmapContextCreate(bitmapData, // 4
+static CGContextRef createBitmapContext(int pixelsWide, int pixelsHigh) {
+    const auto bitmapBytesPerRow = (pixelsWide * 3); // 1
+    const auto colorSpace = CGColorSpaceCreateDeviceGray(); // 2
+    const auto context = CGBitmapContextCreate(nullptr, // 4
         pixelsWide, pixelsHigh,
         8, // bits per component
         bitmapBytesPerRow, colorSpace, kCGImageAlphaNone);
-    if (context == NULL) {
-        free(bitmapData); // 5
-        fprintf(stderr, "Context not created!");
-        return NULL;
-    }
-    CGColorSpaceRelease(colorSpace); // 6
-    return context; // 7
+    CGColorSpaceRelease(colorSpace);
+    return context;
 }
 
 @implementation AppDelegate
@@ -75,7 +57,7 @@ CGContextRef MyCreateBitmapContext(int pixelsWide, int pixelsHigh)
     CGImageRef image = CGImageSourceCreateImageAtIndex(imageSource, 0, nil);
     NSLog(@"image width: %zu", CGImageGetWidth(image));
     CGContextRef context =
-        MyCreateBitmapContext(CGImageGetWidth(image), CGImageGetHeight(image));
+        createBitmapContext(CGImageGetWidth(image), CGImageGetHeight(image));
     CGContextSetRGBFillColor(context, 1, 1, 1, 1);
     CGContextFillRect(context, CGRectMake(0, 0, 100, 100));
     CGContextSetRGBFillColor(context, 1, 1, 1, 1);
@@ -86,20 +68,17 @@ CGContextRef MyCreateBitmapContext(int pixelsWide, int pixelsHigh)
     CGContextFillRect(context, CGRectMake(100, 0, 100, 100));
     CGImageRef mask = CGBitmapContextCreateImage(context);
     CGImageRef masked = CGImageCreateWithMask(image, mask);
-    if (masked == nil) {
-        NSLog(@"Masked is nil");
-    }
     // CGImageRelease(masked);
-    // CGImageRelease(mask);
+    CGImageRelease(mask);
     CGContextRelease(context);
-    // CGImageRelease(image);
+    CGImageRelease(image);
     CFRelease(imageSource);
 
     NSTabViewController *controller = [[NSTabViewController alloc] init];
     [controller setTabStyle:NSTabViewControllerTabStyleUnspecified];
     NSWindow *window = [NSWindow windowWithContentViewController:controller];
     [window makeKeyAndOrderFront:nil];
-    View *view = [[View alloc] initWithImage:masked];
+    ImageView *view = [[ImageView alloc] initWithImage:masked];
     [window.contentView addSubview:view];
 }
 
@@ -108,6 +87,7 @@ CGContextRef MyCreateBitmapContext(int pixelsWide, int pixelsHigh)
 }
 
 - (BOOL)applicationSupportsSecureRestorableState:(NSApplication *)app {
+    return YES;
 }
 
 @end
