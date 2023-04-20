c711c5e83e6500c2d15bdead92314d8158ce6ffe
Seth Bashford <seth.bashford@boystown.org>
2023-04-20 10:11:08 -0500

    preparing to incorporate impl

---
+++ macos/masking-images.h
@@ -0,0 +1,48 @@
+#ifndef AV_SPEECH_IN_NOISE_MACOS_MASKING_IMAGES_H_
+#define AV_SPEECH_IN_NOISE_MACOS_MASKING_IMAGES_H_
+
+#include <av-speech-in-noise/ui/RevealImage.hpp>
+
+#include <CoreGraphics/CoreGraphics.h>
+#include <ImageIO/ImageIO.h>
+
+#import <AppKit/AppKit.h>
+
+#include <memory>
+
+namespace av_speech_in_noise {
+class ScopedBitmapContext {
+  public:
+    explicit ScopedBitmapContext(CGImageRef image);
+    auto operator=(ScopedBitmapContext &&other) noexcept
+        -> ScopedBitmapContext &;
+    ~ScopedBitmapContext();
+
+    CGContextRef context;
+};
+
+class ScopedImage {
+  public:
+    explicit ScopedImage(CGImageSourceRef imageSource);
+
+    ~ScopedImage();
+    CGImageRef image;
+};
+
+class MaskedCoreGraphicsImage : public NormallyMaskedImage {
+    MaskedCoreGraphicsImage(NSWindow *window, NSString *imageName);
+    auto width() -> double override;
+    auto height() -> double override;
+    void reset();
+    void reveal(ImageRegion region) override;
+    void addMaskedImageViewToWindow();
+
+  private:
+    ScopedImage image;
+    std::unique_ptr<ScopedBitmapContext> context;
+    NSWindow *window;
+    NSView *lastImageView;
+};
+}
+
+#endif
--- macos/masking-images.mm
+++ macos/masking-images.mm
@@ -1,6 +1,8 @@
 // https://developer.apple.com/library/archive/documentation/GraphicsImaging/Conceptual/drawingwithquartz2d/dq_context/dq_context.html#//apple_ref/doc/uid/TP30001066-CH203-CJBHBFFE
 // https://developer.apple.com/library/archive/documentation/GraphicsImaging/Conceptual/drawingwithquartz2d/dq_images/dq_images.html#//apple_ref/doc/uid/TP30001066-CH212-TPXREF101
 
+#include "masking-images.h"
+
 #include <AppKit/AppKit.h>
 #include <CoreFoundation/CoreFoundation.h>
 #include <Foundation/Foundation.h>
@@ -35,44 +37,41 @@ class ScopedMaskedImage {
 
 class ScopedBitmapImage {
   public:
-    ScopedBitmapImage(CGContextRef context)
+    explicit ScopedBitmapImage(CGContextRef context)
         : image{CGBitmapContextCreateImage(context)} {
         if (image == nullptr)
             throw std::runtime_error{"Unable to create bitmap image."};
     }
 
     ~ScopedBitmapImage() { CGImageRelease(image); }
+
     CGImageRef image;
 };
 
 class ScopedImageSource {
   public:
-    ScopedImageSource(CFURLRef url)
+    explicit ScopedImageSource(CFURLRef url)
         : imageSource{CGImageSourceCreateWithURL(url, nullptr)} {
         if (imageSource == nullptr)
             throw std::runtime_error{"Unable to create image source."};
     }
 
     ~ScopedImageSource() { CFRelease(imageSource); }
+
     CGImageSourceRef imageSource;
 };
+ScopedImage::ScopedImage(CGImageSourceRef imageSource)
+    : image{CGImageSourceCreateImageAtIndex(imageSource, 0, nullptr)} {
+    if (imageSource == nullptr)
+        throw std::runtime_error{"Unable to create image."};
+}
 
-class ScopedImage {
-  public:
-    ScopedImage(CGImageSourceRef imageSource)
-        : image{CGImageSourceCreateImageAtIndex(imageSource, 0, nullptr)} {
-        if (imageSource == nullptr)
-            throw std::runtime_error{"Unable to create image."};
-    }
-
-    ~ScopedImage() { CGImageRelease(image); }
-    CGImageRef image;
-};
+ScopedImage::~ScopedImage() { CGImageRelease(image); }
 
 static CGContextRef createBitmapContext(int pixelsWide, int pixelsHigh) {
     const auto bitmapBytesPerRow = (pixelsWide * 3); // 1
-    const auto colorSpace = CGColorSpaceCreateDeviceGray(); // 2
-    const auto context = CGBitmapContextCreate(nullptr, // 4
+    auto *const colorSpace = CGColorSpaceCreateDeviceGray(); // 2
+    auto *const context = CGBitmapContextCreate(nullptr, // 4
         pixelsWide, pixelsHigh,
         8, // bits per component
         bitmapBytesPerRow, colorSpace, kCGImageAlphaNone);
@@ -80,37 +79,27 @@ static CGContextRef createBitmapContext(int pixelsWide, int pixelsHigh) {
     return context;
 };
 
-class ScopedBitmapContext {
-  public:
-    ScopedBitmapContext(CGImageRef image)
-        : context{createBitmapContext(
-              CGImageGetWidth(image), CGImageGetHeight(image))} {
-        if (context == nullptr)
-            throw std::runtime_error{"Unable to create bitmap context"};
-    }
+ScopedBitmapContext::ScopedBitmapContext(CGImageRef image)
+    : context{createBitmapContext(
+          CGImageGetWidth(image), CGImageGetHeight(image))} {
+    if (context == nullptr)
+        throw std::runtime_error{"Unable to create bitmap context"};
+}
 
-    ScopedBitmapContext &operator=(ScopedBitmapContext &&other) {
-        if (this != &other) {
-            CGContextRelease(context);
-            context = other.context;
-            other.context = nullptr;
-        }
-        return *this;
+auto ScopedBitmapContext::operator=(ScopedBitmapContext &&other) noexcept
+    -> ScopedBitmapContext & {
+    if (this != &other) {
+        CGContextRelease(context);
+        context = other.context;
+        other.context = nullptr;
     }
+    return *this;
+}
 
-    ~ScopedBitmapContext() {
-        if (context != nullptr)
-            CGContextRelease(context);
-    }
-    CGContextRef context;
-};
-
-struct ImageRegion {
-    double x;
-    double y;
-    double width;
-    double height;
-};
+ScopedBitmapContext::~ScopedBitmapContext() {
+    if (context != nullptr)
+        CGContextRelease(context);
+}
 
 static auto imageResource(NSString *imageName) -> NSURL * {
     NSURL *url = [[NSBundle mainBundle] URLForImageResource:imageName];
@@ -120,51 +109,49 @@ static auto imageResource(NSString *imageName) -> NSURL * {
     return url;
 }
 
-class TBD {
-    TBD(NSWindow *window, NSString *imageName)
-        : image{ScopedImage{
-              ScopedImageSource{(__bridge CFURLRef)(imageResource(imageName))}
-                  .imageSource}},
-          window{window} {
-        reset();
-    }
-
-    auto width() -> double { return CGImageGetWidth(image.image); }
-
-    auto height() -> double { return CGImageGetHeight(image.image); }
-
-    void reset() {
-        context = std::make_unique<ScopedBitmapContext>(image.image);
-    }
-
-    void reveal(ImageRegion region) {
-        CGContextSetRGBFillColor(context->context, 1, 1, 1, 1);
-        CGContextFillRect(context->context,
-            CGRectMake(region.x, region.y, region.width, region.height));
-
-        addMaskedImageViewToWindow();
-    }
-
-    void addMaskedImageViewToWindow() {
-        const auto mask{ScopedBitmapImage{context->context}};
-        const auto imageView = [[ImageView alloc]
-            initWithImage:std::make_unique<ScopedMaskedImage>(
-                              image.image, mask.image)];
-
-        if (lastImageView != nil)
-            [lastImageView removeFromSuperview];
-        lastImageView = imageView;
-
-        [window.contentView addSubview:imageView];
-    }
-
-  private:
-    ScopedImage image;
-    std::unique_ptr<ScopedBitmapContext> context;
-    NSWindow *window;
-    NSView *lastImageView;
-};
+MaskedCoreGraphicsImage::MaskedCoreGraphicsImage(
+    NSWindow *window, NSString *imageName)
+    : image{ScopedImage{
+          ScopedImageSource{(__bridge CFURLRef)(imageResource(imageName))}
+              .imageSource}},
+      window{window} {
+    reset();
 }
+
+auto MaskedCoreGraphicsImage::width() -> double {
+    return CGImageGetWidth(image.image);
+}
+
+auto MaskedCoreGraphicsImage::height() -> double {
+    return CGImageGetHeight(image.image);
+}
+
+void MaskedCoreGraphicsImage::reset() {
+    context = std::make_unique<ScopedBitmapContext>(image.image);
+}
+
+void MaskedCoreGraphicsImage::reveal(ImageRegion region) {
+    CGContextSetRGBFillColor(context->context, 1, 1, 1, 1);
+    CGContextFillRect(context->context,
+        CGRectMake(region.x, region.y, region.width, region.height));
+
+    addMaskedImageViewToWindow();
+}
+
+void MaskedCoreGraphicsImage::addMaskedImageViewToWindow() {
+    const auto mask{ScopedBitmapImage{context->context}};
+    const auto imageView =
+        [[ImageView alloc] initWithImage:std::make_unique<ScopedMaskedImage>(
+                                             image.image, mask.image)];
+
+    if (lastImageView != nil)
+        [lastImageView removeFromSuperview];
+    lastImageView = imageView;
+
+    [window.contentView addSubview:imageView];
+}
+}
+
 @implementation ImageView {
     std::unique_ptr<av_speech_in_noise::ScopedMaskedImage> _image;
 }
@@ -172,7 +159,7 @@ class TBD {
     (std::unique_ptr<av_speech_in_noise::ScopedMaskedImage>)image {
     self = [super initWithFrame:NSMakeRect(0, 0, CGImageGetWidth(image->masked),
                                     CGImageGetHeight(image->masked))];
-    if (self) {
+    if (self != nullptr) {
         _image = std::move(image);
     }
     return self;
